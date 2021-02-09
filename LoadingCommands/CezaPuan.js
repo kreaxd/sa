82b51820232b4f2055665efc0fcc17467ec69414
Glitch (hello-express) <none>
2021-02-09 22:48:31 +0000

    🙈🐚 Checkpoint ./LoadingCommands/CezaPuan.js:1208823/4:15912651/459

--- LoadingCommands/CezaPuan.js
+++ LoadingCommands/CezaPuan.js
@@ -1,5 +1,7 @@
-module.exports.operate = async ({client, msg, args,auth, author}, fetch = require('node-fetch'), { GuildMember } = require("discord.js"), Database = require("../Models/Restriction.js"), Schema = require("../Models/Member.js")) => {
-
+module.exports.operate = async ({client, msg, args,auth, author}, fetch = require('node-fetch'), Schema = require("../Models/Member.js")) => {
+  let member = (msg.mentions.users.first()) || await (await fetch(`https://discord.com/api/users/${args[0]}`, {method:'GET', headers: {'Authorization': 'Bot ' + client.token}})).json();
+  if ((!member) || (Object.keys(member).length == 1)) return client.message(client.noMember(msg), msg.channel.id, 6500);
+  Schema.findOne({SunucuID: msg.guild.id, userID: member.id})
   };
     
   module.exports.help = {
