a1d0727ef08da029d3b8a751d82fd2b21e4bf219
Glitch (hello-express) <none>
2021-02-09 23:00:31 +0000

    🐽🎲 Checkpoint ./LoadingCommands/CezaPuan.js:1208823/55:15912651/439 ./LoadingCommands/BanKaldır.js:15912651/1461

--- LoadingCommands/BanKaldır.js
+++ LoadingCommands/BanKaldır.js
@@ -2,15 +2,21 @@ module.exports.operate = async ({client, msg, args,auth, author}, Database = req
 if ((!author.roles.cache.some(r => auth.Perms.BanAuth.includes(r.id))) & (!author.permissions.has("ADMINISTRATOR"))) return;
 if (!args[0]) return client.message(client.noMember(msg), msg.channel.id, 6500);
 let reasonn = args.slice(1).join(" ") || "Sebep Girilmedi";
-msg.guild.fetchBan(args[0]).then(({ user, reason }) => {
+msg.guild.fetchBan(args[0]).then(async ({ user, reason }) => {
 msg.guild.members.unban(user.id, `${author.tag} tarafından banı açıldı.`);
 msg.channel.send(user.tag + " adlı kullanıcının yasağı kaldırıldı.")
 client.message(`${user.tag} adlı kullanıcının banı başarılı bir şekilde kaldırıldı. Sebep: ${reasonn}\nBanı kaldıran kişi: ${author}`, msg.channel.id);
 msg.react(client.react("duztik")).catch(() => { });
+let Data = await Database.find({Activity: true, userID: user.id, Type: "BAN" });
+if ((Data.length <= 0)) return null;
+Data.forEach(x => {
+x.Activity = false;
+x.save();
+});
 }).catch(err => { msg.channel.send("**Hata**: " + err.message);                 
 });
 };
-    
+ 
   module.exports.help = {
     name: "unban",
     alias: []
--- LoadingCommands/CezaPuan.js
+++ LoadingCommands/CezaPuan.js
@@ -1,12 +1,15 @@
 module.exports.operate = async ({client, msg, args,auth, author}, fetch = require('node-fetch'), Schema = require("../Models/Member.js")) => {
   let member = (msg.mentions.users.first()) || await (await fetch(`https://discord.com/api/users/${args[0]}`, {method:'GET', headers: {'Authorization': 'Bot ' + client.token}})).json();
   if ((!member) || (Object.keys(member).length == 1)) return client.message(client.noMember(msg), msg.channel.id, 6500);
-  Schema.findOne({SunucuID: msg.guild.id, userID: member.id})
+  let cezaPuan = await Schema.findOne({SunucuID: msg.guild.id, userID: member.id})
+  if (!cezaPuan) return client.message(`\`\`\`Bu kullanıcı herhangi bir ceza puanına sahip değil\`\`\``, msg.channel.id, 6500);
+  client.message(`<@${member.id}> \`\`\`Bu kullanıcının toplam ceza puanı ${cezaPuan.CezaPuan}.\`\`\``, msg.channel.id, 6500);
+  
   };
     
   module.exports.help = {
-    name: "ban",
-    alias: ["bb", "maze", "kimsebas", "kimsebaş"]
+    name: "cezapuan",
+    alias: []
   };
  
 
