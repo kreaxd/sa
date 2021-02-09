cb856f15ad3ba0d4510d1708c6a4569376f17e7d
Glitch (hello-express) <none>
2021-02-09 14:54:25 +0000

    🚟🏗 Checkpoint ./LoadingCommands/Rolsüz.js:15912651/285 ./LoadingCommands/YtSay.js:15912651/1851

---
+++ LoadingCommands/YtSay.js
@@ -0,0 +1,9 @@
+module.exports.operate = async ({client, msg, args, author, auth}) => {
+  if (!author.permissions.has("ADMINISTRATOR")) return;
+
+};
+  
+  module.exports.help = {
+    name: "say",
+    alias: []
+  };
