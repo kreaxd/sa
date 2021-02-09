35a368d0be90018606c175a4605fdcc750e0939a
Glitch (hello-express) <none>
2021-02-09 17:50:23 +0000

    🏋🍱 Checkpoint ./LoadingCommands/Sorgu.js:15912651/2340

--- LoadingCommands/Sorgu.js
+++ LoadingCommands/Sorgu.js
@@ -1,14 +1,26 @@
 module.exports.operate = async ({client, msg, args, auth, author}, Database = require("../Models/Restriction.js")) => { 
   if (!author.hasPermission("ADMINISTRATOR")) return;
   let member = args[0];
+  let text;
   if ((!member) || (isNaN(member))) return client.message(client.embed(`Geçerli bir ceza sayısı giriniz.`, msg), msg.channel.id, 6500);
   let xd = await Database.findOne({CezaID: member})
   if (!xd) return client.message(client.embed(`Bu sayıya ait herhangi bir ceza bilgisi bulunmamaktadır.`, msg), msg.channel.id, 6500);
-  msg.channel.send(client.embed(`• Cezayı Alan: <@${xd.userID}> - (\`${xd.userID}\`) 
-  • Cezayı Veren: <@${xd.Author}> - (\`${xd.Author}\`)
-  • Ceza Sebebi: ${xd.Reason}
-  • Ceza Tarihi: ${client.getDate(xd.DateNoW)}
-  • Ceza Türü: ${xd.Type}`,msg))
+  if (["MUTE", "VOICEMUTE"].includes(xd.Type)) {
+    text = `• Cezayı Alan: <@${xd.userID}> - (\`${xd.userID}\`) 
+• Cezayı Veren: <@${xd.Author}> - (\`${xd.Author}\`)
+• Ceza Sebebi: ${xd.Reason}
+• Ceza Tarihi: ${client.toDate(xd.DateNoW)}
+• Ceza Bitiş Tarihi: ${client.toDate(xd.FinishDate)}
+• Ceza Türü: ${xd.Type}`
+  } else { 
+  text = `• Cezayı Alan: <@${xd.userID}> - (\`${xd.userID}\`) 
+• Cezayı Veren: <@${xd.Author}> - (\`${xd.Author}\`)
+• Ceza Sebebi: ${xd.Reason}
+• Ceza Tarihi: ${client.toDate(xd.DateNoW)}
+• Ceza Türü: ${xd.Type}`
+  }
+  msg.channel.send(client.embed(text, msg))
+  
 };
 
 module.exports.help = {
