974fcf25ebe9ab393db1c91ad3d4645b3d73f34e
Glitch (hello-express) <none>
2021-02-09 14:42:25 +0000

    🌠🐺 Checkpoint ./LoadingCommands/Rolsüz.js:1208823/608:15912651/195 ./authorization.json:1208823/20

--- LoadingCommands/Rolsüz.js
+++ LoadingCommands/Rolsüz.js
@@ -1,12 +1,19 @@
 module.exports.operate = async ({client, msg, args,auth, author}) => {
     if (!author.permissions.has("ADMINISTRATOR")) return;
-    let rolsuz = msg.guild.members.cache.filter(x => x)
-
-};
+    let rolsuz = msg.guild.members.cache.filter(m => (m.roles.cache.size) == 1)
+    if(args[0] == "ver") {
+        rolsuz.forEach(r => {
+        r.roles.add(auth.Perms.Unregister).catch(() => { });
+        })
+    msg.channel.send({embed: {description: "Sunucuda rolü olmayan "+ rolsuz.size +" kişiye kayıtsız rolü verdim."}})
+} else if(!args[0]) {
+    msg.channel.send({embed: {description: "Sunucuda rolü olmayan "+ rolsuz.size +" kişi bulunuyor. Bu kişilere kayıtsız rolü vermek için __[.rolsüz ver]__ komudunu uygulayın."}})
+}
+}
     
   module.exports.help = {
-    name: "jail",
-    alias: ["cezalı", "cezali"]
+    name: "rolsuz",
+    alias: ["rolsuz", "rolsuzler"]
   };
 
 
