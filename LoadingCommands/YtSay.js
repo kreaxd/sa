ddcf86951723f6f173a7d2ae67f266512429da86
Glitch (hello-express) <none>
2021-02-09 15:41:23 +0000

    🌼👩‍👩‍👦 Checkpoint ./LoadingCommands/YtSay.js:1208823/115:15912651/13

--- LoadingCommands/YtSay.js
+++ LoadingCommands/YtSay.js
@@ -7,8 +7,8 @@ module.exports.operate = async ({client, msg, args, author, auth}) => {
     yetkililer.map(u => u.user);
     msg.channel.send(`Aktif Olup Seste Olmayan Yetkililer:\n ${yetkililer.join(", \n")}`);
   } else if (["dm", "dm-at"].includes(args[0])) {
-    yetkililer.forEach(member => member.send().catch(err => {
-    msg.channel.send()
+    yetkililer.forEach(member => member.send(``).catch(err => {
+    msg.channel.send(`${member} adlı üyeye dmden mesaj atamıyorum. Müsait isen public odalara değil isen alone odalarına geçiş yapabilirsin.`)
     }))
   }
 
