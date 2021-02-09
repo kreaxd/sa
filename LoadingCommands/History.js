e98ad07f042bd986d11a0dfd51fa575efb36d4f9
Glitch (hello-express) <none>
2021-02-09 16:16:23 +0000

    👬💡 Checkpoint ./LoadingCommands/Isim.js:15912651/1248 ./LoadingCommands/History.js:15912651/1717

---
+++ LoadingCommands/History.js
@@ -0,0 +1,15 @@
+module.exports.operate = async ({client, msg, args, member, author, auth}, Database = require("../Models/Member.js")) => { 
+  if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) & (!author.permissions.has("ADMINISTRATOR"))) return;
+    if (!member) return client.message(client.noMember(msg), msg.channel.id, 6500);
+  Database.findOne({SunucuID: msg.guild.id, userID: member.id}, async (err, res) => {
+    if (!res) return client.message(client.embed(`Kullanıcının kayıt geçmişinde herhangi bir kayıt bulunmuyor!`,msg),msg.channel.id,6500);
+    res = res.History.reverse();
+    const History = res.map((e, i) => `\`${i + 1}) - ${e.Name}\` (<@&${e.Roles}> - <@${e.Author}> - \`${e.Author}\`)`);
+    client.message(client.embed(`${member} kullanıcısının tüm kayıt verileri aşağıda listelenmiştir listelere bakarak kayıt ediniz sorun görüyorsanız üst yetkililere ulaşın!\n\n${History.chunk(20).join(",\n")}`, msg), msg.channel.id, 12000);
+  });
+};
+
+module.exports.help = {
+  name: "isimler",
+  alias: ["history","geçmiş"]
+};
--- LoadingCommands/Isim.js
+++ LoadingCommands/Isim.js
@@ -1,17 +1,21 @@
-
 module.exports.operate = async ({client, msg, args, member, auth, author}) => {
-    if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) & (!author.permissions.has("ADMINISTRATOR"))) return;
+  if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) & (!author.permissions.has("ADMINISTRATOR"))) return;
   if (!member) return client.message(client.noMember(msg), msg.channel.id, 6500);
   const isim = args.slice(1).filter(arg => isNaN(arg)).map(arg => arg.charAt(0).toUpperCase() + arg.slice(arg.charAt(0).length).toLowerCase()).join(" ");
   const yas = args.slice(1).filter(arg => !isNaN(arg))[0];
-  const tag = member.user.username.includes(auth.Tags.RealTag) ? auth.Tags.RealTag : (auth.Tags.FakeTag === "" ? auth.Tags.RealTag : auth.Tags.FakeTag;
-  uye.setNickname(`${tag} ${isim} ${cfg.register.ayrac} ${yas}`);
-  if(!isim || !yaş) return client.mesaj(message.channel.id,5500,randm.setDescription(`${react("iptal")} | Geçerli bir isim ve yaş belirtmelisin!`));
+  const tag = member.user.username.includes(auth.Tags.RealTag) ? auth.Tags.RealTag : (auth.Tags.FakeTag === "" ? auth.Tags.RealTag : auth.Tags.FakeTag);
+  if ((!isim) && (!yas)) return client.message(client.embed(`${client.react("iptal")} | Geçerli bir isim ya da yaş girmelisin.` ,msg), msg.channel.id, 6500);
+  if ((isim) && (!yas)) {
+    member.setNickname(`${tag} ${isim}`)
+  } else if ((isim) || (yas)) {
+    member.setNickname(`${tag} ${isim} | ${yas}`);
+  };
+  msg.react(client.react("duztik")).catch(() => { })
   };
     
   module.exports.help = {
-    name: "jail",
-    alias: ["cezalı", "cezali"]
+    name: "isim",
+    alias: ["i", "nick"]
   };
 
 
