5ee502d8a935c34fb89de5a9b51e17b61ce226de
Glitch (hello-express) <none>
2021-02-09 11:41:31 +0000

    🏘🎈 Checkpoint ./LoadingCommands/Afk.js:15912651/2126 ./LoadingCommands/Uyar.js:15912651/17 ./authorization.json:15912651/29

---
+++ LoadingCommands/Afk.js
@@ -0,0 +1,30 @@
+module.exports.operate = async ({client, msg, args, author}, Database = require("../Models/Member.js")) => {
+    if (client.AFKLAR2.has(author.id)) return null;
+    let reason = args.join(" ") || "Şu anda AFK'yım en kısa sürede döneceğim.";
+    if ((client.reklamcilar.has(author.id)) && (reason.includes("discord.gg") || reason.includes("@everyone") || reason.includes("@here") || reason.includes(msg.mentions.roles.first()))) return msg.guild.members.ban(author.id, { reason: "reklam", days: 7});
+    if (reason.includes("discord.gg") || reason.includes("@everyone") || reason.includes("@here") || reason.includes(msg.mentions.roles.first())) {
+      msg.delete({timeout: 10});
+      msg.reply("`AFK` **moduna giriş yaparken link veya etiket (`@everyone, @Rol`) atamazsın.**", { disableMentions: "everyone" }).then(m => m.delete({ timeout: 3500 }));
+      client.reklamcilar.add(author.id);
+      return;
+    };
+    Database.findOne({SunucuID: msg.guild.id, userID: author.id}, async (err, res) => {
+      if (!res) {
+        new Database({SunucuID: msg.guild.id, userID: author.id, AFK: { mod: true, reason: reason, date: Date.now() } }).save();
+        if ((author.manageable) && (author.displayName.length < 28)) author.setNickname(`[AFK] ${author.displayName}`).catch(err => msg.channel.send(err.message));
+        client.message(client.embed(`Başarıyla AFK moduna geçiş yaptın.` , msg),msg.channel.id, 4500);
+      } else {
+        res.AFK = { mod: true, reason: reason, date: Date.now() };
+        res.save();
+        if ((author.manageable) && (author.displayName.length < 28)) author.setNickname(`[AFK] ${author.displayName}`).catch(err => msg.channel.send(err.message));
+        client.message(client.embed(`Başarıyla AFK moduna geçiş yaptın.` , msg),msg.channel.id, 4500);
+      };
+    });
+    if (client.AFKLAR2.has(author.id)) setTimeout(() => client.AFKLAR2.delete(author.id), client.getDate(7, "saniye"));
+    if (client.reklamcilar.has(author.id)) setTimeout(() => client.reklamcilar.delete(author.id), client.getDate(10, "saniye"));
+  };
+  
+  module.exports.help = {
+    name: "afk",
+    alias: ["afık"]
+  };
--- LoadingCommands/Uyar.js
+++ LoadingCommands/Uyar.js
@@ -27,7 +27,7 @@ module.exports.operate = async ({client, msg, args, member, author, auth}, Schem
      client.message({embed: { 
     author: { name: msg.member.user.tag, icon_url:  msg.member.user.displayAvatarURL({dynamic:true}) }, 
     description: `${member} (\`${member.user.tag} - ${member.id}\`) üyesi ${author} tarafından ${UyarıSayısı}. uyarısını aldı. Sebep: ${reason}`, 
-    color: client.renk[Math.floor(Math.random() * client.renk.length)]}}, auth.Logs.MuteLog)
+    color: client.renk[Math.floor(Math.random() * client.renk.length)]}}, auth.Logs.WarningLog)
      }
     });
   };
