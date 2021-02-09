afa69cf34f4c3d2b09d90d29d12f887469c4f3f6
Glitch (hello-express) <none>
2021-02-09 15:06:25 +0000

    🎉🛢 Checkpoint ./LoadingCommands/YtSay.js:1208823/559:15912651/719

--- LoadingCommands/YtSay.js
+++ LoadingCommands/YtSay.js
@@ -1,5 +1,16 @@
 module.exports.operate = async ({client, msg, args, author, auth}) => {
   if (!author.permissions.has("ADMINISTRATOR")) return;
+  let yetkililer = msg.guild.members.cache.filter(u => {
+  return (u.roles.cache.some(r => auth.Perms.YTRoles.includes(r.id)) && !u.voice.channel && !u.presence.status !== "offline")
+  });
+  if (!args[0]) {
+    yetkililer.map(u => u.user);
+    msg.channel.send(`Aktif Olup Seste Olmayan Yetkililer:\n ${yetkililer.join(", \n")}`);
+  } else if (["dm", "dm-at"].includes(args[0])) {
+    yetkililer.forEach(member => member.send().catch(err => {
+    msg.channel.send()
+    }))
+  }
 
 };
   
