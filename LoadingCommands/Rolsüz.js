db867ce597adcd44c481a5009d88ccbbb5235b8c
Glitch (hello-express) <none>
2021-02-09 14:30:24 +0000

    🚆☎️ Checkpoint ./LoadingCommands/Rolsüz.js:1208823/686:15912651/87

--- LoadingCommands/Rolsüz.js
+++ LoadingCommands/Rolsüz.js
@@ -1,6 +1,6 @@
 module.exports.operate = async ({client, msg, args,auth, author}) => {
-    if (!author.permissions.has("ADMINISTRATOR") return;
-    let rolsuz = msg.guild.members.cache.filter.
+    if (!author.permissions.has("ADMINISTRATOR")) return;
+    let rolsuz = msg.guild.members.cache.filter(x => x)
 
 };
     
