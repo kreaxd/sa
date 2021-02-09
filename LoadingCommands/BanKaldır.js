96aece0552b4aa43068caafbb15a1c7c39b73eb8
Glitch (hello-express) <none>
2021-02-09 22:36:32 +0000

    🎟🌷 Checkpoint ./LoadingCommands/BanKaldır.js:15912651/249 ./LoadingCommands/CezaPuan.js:1208823/5169

--- LoadingCommands/BanKaldır.js
+++ LoadingCommands/BanKaldır.js
@@ -1,11 +1,11 @@
-module.exports.operate = async ({client, msg, args,auth, author}, fetch = require('node-fetch'), { GuildMember } = require("discord.js"), Database = require("../Models/Restriction.js"), Schema = require("../Models/Member.js")) => {
+module.exports.operate = async ({client, msg, args,auth, author}, Database = require("../Models/Restriction.js")) => {
 if ((!author.roles.cache.some(r => auth.Perms.BanAuth.includes(r.id))) & (!author.permissions.has("ADMINISTRATOR"))) return;
 if (!args[0]) return client.message(client.noMember(msg), msg.channel.id, 6500);
-let reason = args.slice(1).join(" ") || "Sebep Girilmedi";
+let reasonn = args.slice(1).join(" ") || "Sebep Girilmedi";
 msg.guild.fetchBan(args[0]).then(({ user, reason }) => {
 msg.guild.members.unban(user.id, `${author.tag} tarafından banı açıldı.`);
 msg.channel.send(user.tag + " adlı kullanıcının yasağı kaldırıldı.")
-client.message(`${user.tag} adlı kullanıcının banı başarılı bir şekilde kaldırıldı. Sebep: ${reason}\nBanı kaldıran kişi: ${author}`, msg.channel.id);
+client.message(`${user.tag} adlı kullanıcının banı başarılı bir şekilde kaldırıldı. Sebep: ${reasonn}\nBanı kaldıran kişi: ${author}`, msg.channel.id);
 msg.react(client.react("duztik")).catch(() => { });
 }).catch(err => { msg.channel.send("**Hata**: " + err.message);                 
 });
---
+++ LoadingCommands/CezaPuan.js
@@ -0,0 +1,11 @@
+module.exports.operate = async ({client, msg, args,auth, author}, fetch = require('node-fetch'), { GuildMember } = require("discord.js"), Database = require("../Models/Restriction.js"), Schema = require("../Models/Member.js")) => {
+
+  };
+    
+  module.exports.help = {
+    name: "ban",
+    alias: ["bb", "maze", "kimsebas", "kimsebaş"]
+  };
+ 
+
+ 
