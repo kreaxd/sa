3dcb05d9a2f0d688099cacaf7b19a91e67c45f16
Glitch (hello-express) <none>
2021-02-10 09:02:34 +0000

    🚁📖 Checkpoint ./LoadingCommands/Çek.js:1208823/167:15912651/1252

--- LoadingCommands/Çek.js
+++ LoadingCommands/Çek.js
@@ -3,31 +3,35 @@ if (!author.voice.channel) return client.message(client.embed("Komutu kullanmak 
 if (!member) return client.message(client.embed("Çekmek istediğin bir kullanıcıyı seçmelisin!",msg), msg.channel.id, 6500);
 if (!member.voice.channel || author.voice.channelID == member.voice.channelID ) return client.message(client.embed('Kullanıcı ile aynı kanaldasınız ya da kullanıcı bir sesli kanalda değil!',msg),msg.channel.id,6500);
 let reason = args.slice(1).join(" ") || 'Sebep belirtilmedi.'
-if ((!author.roles.cache.some(r => auth.Perms.TransportAuth.includes(r.id))) & (!author.permissions.has("ADMINISTRATOR"))) {
-await member.voice.setChannel(author.voice.channelID);
-msg.react(client.react("duztik"))
+if ((author.roles.cache.some(r => auth.Perms.TransportAuth.includes(r.id))) & (author.permissions.has("ADMINISTRATOR"))) {
+await member.voice.setChannel(author.voice.channelID).catch(() => { });
+msg.react(client.react("duztik")).catch(() => { });
 } else {
-client.mesaj(message.channel.id,30000,embed.setDescription(`${message.author} seni **${reason}** sebebiyle yanına çağırıyor gitmek ister misin?`).setFooter('30 saniye içinde herhangi bir tepki vermezseniz komut iptal olur.')).then(async msg => {
-await msg.react("✅").catch();
-await msg.react("❌").catch();
-const onay = (reaction, user) => reaction.emoji.name === '✅' && user.id === kingdom.id;
-const red = (reaction, user) => reaction.emoji.name === '❌' && user.id === kingdom.id;
-const tik = msg.createReactionCollector(onay, { time: 30000 });
-const çarpı = msg.createReactionCollector(red, { max: 1, time: 30000 });
+client.message({embed: { 
+color: client.renk[Math.floor(Math.random() * client.renk.length)]},
+author: { name: msg.member.user.tag, icon_url:  msg.member.user.displayAvatarURL({dynamic:true}) }, 
+description: `${author} seni **${reason}** sebebiyle yanına çağırıyor gitmek ister misin?`,
+footer: { text: "30 saniye içinde herhangi bir tepki vermezseniz komut iptal olur."}}).then(async x => {
+await x.react(client.react("duztik")).catch(() => { });
+await x.react(client.react("iptal")).catch(() => { });
+const onay = (reaction, user) => reaction.emoji.id === auth.Reacts.duztik && user.id === member.id;
+const red = (reaction, user) => reaction.emoji.id === auth.Reacts.iptal && user.id === member.id;
+const tik = x.createReactionCollector(onay, { time: 30000 });
+const çarpı = x.createReactionCollector(red, { max: 1, time: 30000 });
 tik.on("collect", r => {
-kingdom.voice.setChannel(message.member.voice.channelID).catch((()=> { return undefined; }));
-msg.delete().catch((()=> { return undefined; }));
-message.react(react("onay")).catch((()=> { return undefined; }));
+member.voice.setChannel(author.voice.channelID).catch(() => { });
+x.delete().catch(() => { });
+msg.react(client.react("duztik")).catch(()=> { });
 })
 çarpı.on("collect", r => {
-msg.delete().catch((()=> { return undefined; }));
-message.react(react("iptal")).catch((()=> { return undefined; }));
+x.delete().catch(()=> { });
+msg.react(client.react("iptal")).catch(()=> { });
 })
 })
 };
 };
   
 module.exports.help = {
-  name: "booster",
+  name: "çek",
   alias: ["zengin"]
 };
