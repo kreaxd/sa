f7a105dd648b0b1f49ab6cc4db73ad1b51f0405d
Glitch (hello-express) <none>
2021-02-10 12:17:21 +0000

    💐🏪 Checkpoint ./LoadingCommands/Taşı.js:15912651/1041

--- LoadingCommands/Taşı.js
+++ LoadingCommands/Taşı.js
@@ -1,25 +1,15 @@
-
-
-if(Channel.type != 'voice') return client.mesaj(message.channel.id,5500,emb.setDescription('Mesaj kanallarına birini taşıyamam.'));
-kingdom.voice.setChannel(Channel).catch((()=> { return undefined; }));
-message.react(react("onay")).catch();
-};
-module.exports.configuration = {
-    name: "taşı",
-    aliases: ["taşı","götür","gönder"],
-    usage: "taşı kişi",
-    description: "taşır"
-};
-
 module.exports.operate = async ({client, msg, member ,args, auth, author}) => { 
 if ((!author.roles.cache.some(r => auth.Perms.TransportAuth.includes(r.id))) || (!author.permissions.has("ADMINISTRATOR"))) return;
 if (!member) return client.message(client.noMember(msg), msg.channel.id, 5000);
 if (!member.voice.channel) return client.message(client.embed("Kullanıcının herhangi bir ses kanalında olması lazım.",msg), msg.channel.id, 6500);
-const Channel = message.guild.channels.cache.get(args[1]);
-if (!Channel) return client.message(client.embed('THerhangi bir kanal belirtmelisin.', msg), msg.channel.id, 6500);
+const Channel = msg.guild.channels.cache.get(args[1]);
+if (!Channel) return client.message(client.embed('Kullanıcıyı taşımak istediğin herhangi bir kanal belirtmelisin.', msg), msg.channel.id, 6500);
+if(Channel.type != 'voice') return client.message(client.embed('Kullanıcıyı taşımak istediğin kanal bir ses kanalı olmak zorunda.', msg), msg.channel.id, 6500);
+member.voice.setChannel(Channel).catch(()=> { });
+msg.react(client.react("duztik")).catch(() => { });
 };
 
 module.exports.help = {
-  name: "sil",
-  alias: ["temizle", "clear"]
+  name: "taşı",
+  alias: ["gönder", "yolla"]
 }; 
