a015e01a11bb87f1b1dfbf97a25f9484e42e0cd4
Glitch (hello-express) <none>
2021-02-10 20:37:11 +0000

    👫👀 Checkpoint ./LoadingCommands/Afk.js:15912651/4 ./watch.json:15912651/10 ./LoadingCommands/YtSay.js:15912651/21

--- LoadingCommands/YtSay.js
+++ LoadingCommands/YtSay.js
@@ -4,8 +4,8 @@ module.exports.operate = async ({client, msg, args, author, auth}) => {
   return (u.roles.cache.some(r => auth.Perms.YTRoles.includes(r.id)) && !u.voice.channel && !u.presence.status !== "offline")
   });
   if (!args[0]) {
-    yetkililer.map(u => u.user);
-    msg.channel.send(`Aktif Olup Seste Olmayan Yetkililer:\n ${yetkililer.join(", \n")}`);
+    let xd = yetkililer.map(u => u.user);
+    msg.channel.send(`Aktif Olup Seste Olmayan Yetkililer:\n ${xd.join(", \n")}`);
   } else if (["dm", "dm-at"].includes(args[0])) {
     yetkililer.forEach(member => member.send(`Merhabalar sunucumuzun ses aktifliğini arttırmak için lütfen müsait isen public odalara değil isen alone odalarına geçer misin?`).catch(err => {
     msg.channel.send(`${member} adlı üyeye dmden mesaj atamıyorum. Müsait isen public odalara değil isen alone odalarına geçiş yapabilirsin.`)
