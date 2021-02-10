bdf28a95f53fae6617b96ccd2f7df924fef3a765
Glitch (hello-express) <none>
2021-02-10 11:36:32 +0000

    🌏🎇 Checkpoint ./LoadingCommands/TagAldır.js:15912651/1108 ./LoadingCommands/YtBilgi.js:1208823/2

--- LoadingCommands/TagAldır.js
+++ LoadingCommands/TagAldır.js
@@ -1,5 +1,9 @@
+let Zaman = new Set();
 module.exports.operate = async ({client, msg, args, member ,author, auth}, Database = require("../Models/Member.js"),{MessageEmbed} = require("discord.js")) => {
 if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
+if (!Zaman[author.id])
+Zaman[author.id] = { kullanim: 0};
+if (Zaman[author.id].kullanim >= 1) return client.message(client.embed(`3 dakikada bir kere bu komudu kullanabilirsin.`, msg), msg.channel.id, 6500);
 if (!member) return client.message(client.noMember(msg), msg.channel.id, 6500);
 let tagarray = [];
 let VeriCheck = await Database.find({SunucuID: msg.guild.id})
@@ -10,6 +14,8 @@ let VeriCheck = await Database.find({SunucuID: msg.guild.id})
   })
 if (tagarray.includes(member.id)) return client.message(client.embed(`Bu kullanıcı başka birisi tarafından tag aldırıldığı için sana ekleyemem.`, msg), msg.channel.id, 6500);
 //if (!member.user.username.includes(auth.Tags.RealTag)) return client.message(client.embed("Kullanıcının isminde tagımız bulunmuyor bu yüzden işlem iptal edildi.",msg),msg.channel.id,6500);
+client.message(client.embed(`Kullanıcıya tag aldırma mesajı başarıyla gönderildi.` ,msg), msg.channel.id, 6500);
+msg.react(client.react("duztik")).catch(() => { });
 member.send({embed: { 
 color: client.renk[Math.floor(Math.random() * client.renk.length)],
 author: { name: msg.member.user.tag, icon_url:  msg.member.user.displayAvatarURL({dynamic:true}) }, 
@@ -30,7 +36,10 @@ if (!Veri) { new Database({SunucuID: msg.guild.id, userID: author.id, Authorized
   Veri.Authorized.TagMembers.push(member.id)
   Veri.save()
 }
-client.message(client.embed(`${author} adlı yetkili ${member} (\`${member.id}\`) adlı kullanıcıya tagımızı (\`${auth.Tags.RealTag}\`) aldırdı ve **+3** puan kazandı.`, msg), auth.Logs.TagLog)
+client.message(client.embed(`${author} adlı yetkili ${member} (\`${member.id}\`) adlı kullanıcıya tagımızı (\`${auth.Tags.RealTag}\`) aldırdı.`, msg), auth.Logs.TagLog)
+if (Zaman[author.id].kullanim >= 1) {
+Zaman[author.id].kullanim = 0;
+}
 });
 çarpı.on("collect", r => {
 x.delete().catch(()=> { });
@@ -41,6 +50,10 @@ x.delete().catch(() => { });
 }).catch(() => { 
   client.message(client.embed(`Bir hata oluştu zaten gönderilmiş bir mesaj var ya da kullanıcının DM kutusu kapalı.`, msg), msg.channel.id, 6500);
 });
+Zaman[author.id].kullanim++;
+
+
+
 };
   
 module.exports.help = {
