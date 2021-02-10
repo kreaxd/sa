fe97a24155605218f4bf88dbf310020017431450
Glitch (hello-express) <none>
2021-02-10 20:49:11 +0000

    🏇🎨 Checkpoint ./LoadingCommands/YtSay.js:15912651/3 ./watch.json:15912651/6 ./LoadingCommands/Say.js:15912651/244

--- LoadingCommands/Say.js
+++ LoadingCommands/Say.js
@@ -1,5 +1,5 @@
 module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed} = require("discord.js")) => {
-  if (!author.permissions.has("ADMINISTRATOR")) return;
+ if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
     const MemberSize = msg.guild.memberCount;
     const TaglıSize = msg.guild.members.cache.filter(u => u.user.username.includes(auth.Tags.RealTag)).size || 0;
     const OnlineSize = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
@@ -9,6 +9,7 @@ module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed
 \`>\` Sunucumuzda toplam **${MemberSize}** üye var.
 \`>\` Toplam **${TaglıSize}** kişi tagımıza sahip.
 \`>\` Sunucumuzda toplam **${OnlineSize}** çevrimiçi üye var.`))
+    msg.react(client.react("duztik")).catch(() => { });
   };
   
   module.exports.help = {
