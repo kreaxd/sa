c2f87c52801c50b79f6abf810f4d82048068ccd2
Glitch (hello-express) <none>
2021-02-10 10:48:33 +0000

    🚙🚙 Checkpoint ./Models/Member.js:15912651/23 ./LoadingCommands/TagAldır.js:15912651/1411

--- LoadingCommands/TagAldır.js
+++ LoadingCommands/TagAldır.js
@@ -1,6 +1,11 @@
 module.exports.operate = async ({client, msg, args, member ,author, auth}, Database = require("../Models/Member.js"),{MessageEmbed} = require("discord.js")) => {
 if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
 if (!member) return client.message(client.noMember(msg), msg.channel.id, 6500);
+Database.find({SunucuID: msg.guild.id}, async (err,res) => { 
+  if ((res) && (res.Authorized)) {
+    msg.channel.send("qwewq")
+  }
+});
 //if (!member.user.username.includes(auth.Tags.RealTag)) return client.message(client.embed("Kullanıcının isminde tagımız bulunmuyor bu yüzden işlem iptal edildi.",msg),msg.channel.id,6500);
 member.send({embed: { 
 color: client.renk[Math.floor(Math.random() * client.renk.length)],
@@ -13,12 +18,13 @@ const onay = (reaction, user) => reaction.emoji.id === auth.Reacts.duztik && use
 const red = (reaction, user) => reaction.emoji.id === auth.Reacts.iptal && user.id === member.id;
 const tik = x.createReactionCollector(onay, { time: 1000 * 60 * 3  });
 const çarpı = x.createReactionCollector(red, { max: 1, time: 1000 * 60 * 3 });
-const Veri = await Database.findOne({SunucuID: msg.guild.id, userID: author.id})
+const Veri = await Database.findOne({SunucuID: msg.guild.id, userID: author.id});
 tik.on("collect", r => {
 x.delete().catch(() => { });
-if (!Veri) { new Database({SunucuID: msg.guild.id, userID: author.id, Authorized: { Tags: 1}}).save()
+if (!Veri) { new Database({SunucuID: msg.guild.id, userID: author.id, Authorized: { Tags: 1, TagMembers: [member.id]}}).save();
 } else {
   Veri.Authorized.Tags++
+  Veri.Authorized.TagMembers.push(member.id)
   Veri.save()
 }
 });
--- Models/Member.js
+++ Models/Member.js
@@ -5,7 +5,7 @@ const member = mongoose.Schema({
   userID: String,
   AFK: Object,
   History: Array,
-  Authorized: { Man: { type: Number, default: 0 }, Woman: { type: Number, default: 0 }, Members: Array, Tags: { type: Number, default: 0 } },
+  Authorized: { Man: { type: Number, default: 0 }, Woman: { type: Number, default: 0 }, Members: Array, Tags: { type: Number, default: 0 }, TagMembers: Array },
   RestNumber: { BanNumber: { type: Number, default: 0 }, JailNumber: { type: Number, default: 0 }, VMuteNumber: { type: Number, default: 0 }, MuteNumber: { type: Number, default: 0 } },
   CezaPuan: { type: Number, default: 0},
   Uyarılar: Array,
