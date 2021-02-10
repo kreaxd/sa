984af8083f7b6e107e1f5c8be53f60e37f9a3d78
Glitch (hello-express) <none>
2021-02-10 16:43:18 +0000

    📼🍙 Checkpoint ./LoadingEvents/AFk.js:15912651/3466

---
+++ LoadingEvents/AFk.js
@@ -0,0 +1,67 @@
+const client = process.client;
+const Database = require("../Models/Member.js");
+const parsems = require("parse-ms");
+const auth = require("../authorization.json");
+
+class AFK {
+  constructor(msg) {
+    this.msg = msg;
+  }
+  
+  AFKcikis(author) {
+    Database.findOne({SunucuID: auth.Guild, userID: this.msg.author.id}, async (err, res) => {
+      if (res) {
+        let afkveri = res.AFK || {};
+        if (afkveri.mod) {
+          res.AFK = {};
+          res.save();
+          if (author.manageable) author.setNickname(author.displayName.replace("[AFK]", "")).catch(err => this.msg.channel.send(err.message));
+          let süre = parsems(Date.now() - afkveri.date);
+          this.msg.channel.send(client.embed("**`AFK` modundan başarıyla çıkış yaptın.**", this.msg)).then(msg => msg.delete({ timeout: 5000 }));
+        } else { };
+      } else { };
+    });
+  }
+  
+  isAFK(uye) {
+    if ((uye.id !== this.msg.author.id) && (!client.AFKLAR.has(uye.id))) {
+      Database.findOne({SunucuID: this.msg.sunucu, userID: uye.id}, async (err, res) => {
+        if (res) {
+          let afkveri = res.AFK || {};
+          if (afkveri.mod) {
+            let süre = parsems(Date.now() - afkveri.date);
+            let reason = afkveri.reason;
+            await client.AFKLAR.add(uye.id);
+            setTimeout(() => client.AFKLAR.delete(uye.id), client.getDate(5, "saniye"));
+            if (süre.days !== 0) {
+              client.message({embed: {description:`${uye} kullanıcısı **${süre.days}** gün **${süre.hours}** saat **${süre.minutes}** dakika önce **AFK** moduna girdi.\nAFK Nedeni: \`${reason}\``}, color: client.favoriRenkler[Math.floor(Math.random() * client.favoriRenkler.length)], timestamp: new Date()}, this.msg.channel.id, 6000);
+              return;
+            } else if (süre.hours !== 0) {
+              client.message({embed: {description:`${uye} kullanıcısı **${süre.hours}** saat **${süre.minutes}** dakika önce **AFK** moduna girdi.\nAFK Nedeni: \`${reason}\``}, color: client.favoriRenkler[Math.floor(Math.random() * client.favoriRenkler.length)], timestamp: new Date()}, this.msg.channel.id, 6000);
+              return;
+            } else if (süre.minutes !== 0) {
+              client.message({embed: {description:`${uye} kullanıcısı **${süre.minutes}** dakika önce **AFK** moduna girdi.\nAFK Nedeni: \`${reason}\``}, color: client.favoriRenkler[Math.floor(Math.random() * client.favoriRenkler.length)], timestamp: new Date()}, this.msg.channel.id, 6000); 
+              return;
+            } else if (süre.seconds !== 0) {
+              client.message({embed: {description:`${uye} kullanıcısı biraz önce **AFK** moduna girdi.\nAFK Nedeni: \`${reason}\``}, color: client.favoriRenkler[Math.floor(Math.random() * client.favoriRenkler.length)], timestamp: new Date()}, this.msg.channel.id, 6000);
+              return;
+            };
+          } else { };
+        } else { };
+      });
+    };
+  }  
+}
+
+function afkControl(message) {
+  if (message.author.bot || message.channel.type === "dm" || message.content.startsWith(prefix) || message.guild.id !== sunucu) return null;
+  let uye = message.guild.member(message.mentions.users.first());
+  let author = message.guild.member(message.author);
+  new AFK(message).AFKcikis(author);
+  if (uye) new AFK(message).isAFK(uye);
+};
+
+module.exports.event = {
+  name: "message",
+  eventOn: message => afkControl(message)
+};
