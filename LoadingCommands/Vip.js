e41f8b132a0e2a201e08a97030f4d3401e70a42a
Glitch (hello-express) <none>
2021-02-11 11:44:08 +0000

    💓☃️ Checkpoint ./LoadingCommands/Vip.js:1208823/990 ./watch.json:1208823/2

--- LoadingCommands/Vip.js
+++ LoadingCommands/Vip.js
@@ -1,15 +1,12 @@
 module.exports.operate = async ({client, msg, member ,args, auth, author}) => { 
-if ((!author.roles.cache.some(r => auth.Perms.TransportAuth.includes(r.id))) || (!author.permissions.has("ADMINISTRATOR"))) return;
+if ((!author.permissions.has("ADMINISTRATOR"))) return;
   if (!member) return client.message(client.uyeEmbed(msg), msg.channel.id, 5500);
-      member.roles.cache.has(cfg.yetki.vip) ? member.roles.remove(cfg.yetki.vip) : uye.roles.add(cfg.yetki.vip);
-const Channel = msg.guild.channels.cache.get(args[1]);
-if (!Channel) return client.message(client.embed('Kullanıcıyı taşımak istediğin herhangi bir kanal belirtmelisin.', msg), msg.channel.id, 6500);
-if(Channel.type != 'voice') return client.message(client.embed(`${client.react("iptal")} | Kullanıcıyı taşımak istediğin kanal bir ses kanalı olmak zorunda.`, msg), msg.channel.id, 6500);
-member.voice.setChannel(Channel).catch(()=> { });
-msg.react(client.react("duztik")).catch(() => { });
+      member.roles.cache.has(auth.Perms.Vip) ? member.roles.remove(auth.Perms.Vip) : member.roles.add(auth.Perms.Vip);
+    msg.react(client.react("duztik"));
+ msg.channel.send({embed: {description:`${member} adlı üyede <@&${auth.Perms.Vip}> permi için gerekli işlemler yapılmıştır.`}})
 };
 
 module.exports.help = {
-  name: "taşı",
-  alias: ["gönder", "yolla"]
+  name: "vip",
+  alias: ["elite"]
 }; 
