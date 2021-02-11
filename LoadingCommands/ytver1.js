703d414b283dfb6f57264cca4877e60eb74cf9fd
Glitch (hello-express) <none>
2021-02-11 16:56:43 +0000

    👗🍗 Checkpoint ./LoadingCommands/ytver1.js:1208823/1112 ./watch.json:1208823/6 ./authorization.json:1208823/43

--- LoadingCommands/ytver1.js
+++ LoadingCommands/ytver1.js
@@ -1,10 +1,14 @@
-module.exports.operate = async ({client, msg, author, uye, auth}, Discord = require("discord.js")) => {
-    if ((!author.roles.cache.some(r => auth.YetkiVer.YetkiliAlım.includes(r.id))) && (!author.hasPermission("MANAGE_ROLES"))) retur
-    if (!uye) return client.message(client.uyeEmbed(msg), msg.channel.id, 5500);
-    await uye.roles.add(auth.YetkiVer.yetkiver1);
-    await uye.roles.add(auth.YetkiVer.yetkiver11);
+module.exports.operate = async ({client, msg, author, member, auth}, Discord = require("discord.js")) => {
+    if ((!author.roles.cache.some(r => auth.YetkiVer.YetkiliAlım.includes(r.id))) && (!author.hasPermission("MANAGE_ROLES"))) return;
+  if (!member) return client.message(client.noMember(msg), msg.channel.id, 6500);
+    await member.roles.add(auth.YetkiVer.yetkiver1);
+    await member.roles.add(auth.YetkiVer.yetkiver11);
     msg.react(client.react("duztik"));
-    await msg.channel.send(`${uye}** adlı üye başarıyla Deneme Yetkisi rolü verilmiştir.**`)
+    await msg.channel.send(`${member}** adlı üye başarıyla Deneme Yetkisi rolü verilmiştir.**`);
+     client.message({embed: { 
+        author: { name: msg.member.user.tag, icon_url:  msg.member.user.displayAvatarURL({dynamic:true}) }, 
+      description: `${author} yetkili tarafından ${member} adlı üye başarıyla Deneme Yetkisi rolü verilmiştir.`,
+      color: client.renk[Math.floor(Math.random() * client.renk.length)]}}, auth.Logs.yetkiliLog); 
   };
 
   module.exports.help = {
