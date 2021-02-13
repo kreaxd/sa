1b8884c26f5f06001518218680349c52c2c6c623
Glitch (hello-express) <none>
2021-02-13 14:13:48 +0000

    🚍🚖 Checkpoint ./LoadingCommands/YtSay.js:1208823/13 ./watch.json:1208823/4

--- LoadingCommands/YtSay.js
+++ LoadingCommands/YtSay.js
@@ -1,12 +1,12 @@
 module.exports.operate = async ({client, msg, args, author, auth}) => {
   if (!author.permissions.has("ADMINISTRATOR")) return;
   let yetkililer = msg.guild.members.cache.filter(u => {
-  return (u.roles.cache.some(r => auth.Perms.YTRoles.includes(r.id)) && !u.voice.channel && !u.presence.status !== "offline")
+  return (u.roles.cache.some(r => auth.Perms.YTRoles.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline")
   });
   if (!args[0]) {
     let xd = yetkililer.map(u => u.user);
     client.chunkArray(xd, 25).forEach(r => {
-      msg.channel.send(`Aktif Olup Seste Olmayan Yetkililer: ${xd.size}\n ${r.join(",")}`);
+      msg.channel.send(`Aktif Olup Seste Olmayan Yetkililer: ${yetkililer.size}\n ${r.join(",")}`);
     });
   } else if (["dm", "dm-at"].includes(args[0])) {
     yetkililer.forEach(member => member.send(`Merhabalar sunucumuzun ses aktifliğini arttırmak için lütfen müsait isen public odalara değil isen alone odalarına geçer misin?`).catch(err => {
