d53537117cf35a0eaf36f7f4fbd4aeb601b28465
Glitch (hello-express) <none>
2021-02-16 09:28:47 +0000

    👘🌿 Checkpoint ./LoadingEvents/Talent.js:15912651/2906 ./watch.json:15912651/2 ./LoadingCommands/Talent.js:15912651/9161 ./authorization.json:15912651/1744

--- LoadingEvents/Talent.js
+++ LoadingEvents/Talent.js
@@ -6,65 +6,22 @@ class Talent {
   }
 
   async TalentPerms() {
+    client.komutlar = ["vip", "elite"];
     if (!Array.isArray(auth.GuildData.Prefixes)) auth.GuildData.Prefixes = [auth.GuildData.Prefixes];
     if (!auth.GuildData.Prefixes.some(x => this.msg.content.startsWith(x.toLowerCase()))) return;
     if (this.msg.author.bot || this.msg.guild.id !== auth.GuildData.GuildID || this.msg.channel.type === "dm") return;
     let args = auth.GuildData.Prefixes.some(x => this.msg.content.substring(x.oLowerCase().length)).split(" ");
+    let command = args[0];
+    args = args.splice(1);
+    let member = this.msg.guild.member(this.msg.mentions.users.first()) || this.msg.guild.members.cache.get(args[0]);
+    let komut = client.komutlar.find(k => k === command);
+    if ((komut) && (komut === "vip" || komut === "elite")) {
+       
+    }
   }
 }
 
 module.exports.event = {
   name: "message",
   eventOn: message => new Talent(message).TalentPerms()
-};
-
-
-  if (!message.content.startsWith(conf.prefix)) return;
-  let args = message.content.substring(conf.prefix.length).split(" ");
-  let command = args[0];
-  args = args.splice(1);
-  let uye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-  if (!uye) return;
-  let komut = client.komutlar.find(k => k.isim === command);
-  if (komut && (komut.isim === "yetkilial1" || komut.isim === "yetkilial2" || komut.isim === "yetkilial3")) {
-    if (!message.member.roles.cache.has("702571625811542047") && !message.member.roles.cache.has(ayar.sahipRolu) && !conf.sahip.some(id => message.author.id === id)) return;
-    uye.roles.add([komut.rol, "729396561691541586", "601851617343701034", "645664873191309314"]);
-    return message.react(client.emojiler.onay);
-  };
-
-  if (komut && (komut.isim === "teyitver")) {
-    if (!message.member.roles.cache.has("626080186588200960") && !message.member.roles.cache.has(ayar.sahipRolu) && !conf.sahip.some(id => message.author.id === id)) return;
-    uye.roles.add(["600429816935874609", "603665487662153738"]);
-    return message.react(client.emojiler.onay);
-  };
-
-  if (komut && komut.isim === "terapist") {
-  if (!message.member.roles.cache.has("701007285983379456") && !message.member.roles.cache.has(ayar.sahipRolu) && !conf.sahip.some(id => message.author.id === id)) return;
-    uye.roles.cache.has(komut.rol) ? uye.roles.remove(komut.rol) : uye.roles.add(komut.rol);
-    return message.react(client.emojiler.onay);
-  };
-  if (komut && (komut.isim === "rehber" || komut.isim === "uyarı1" || komut.isim === "uyarı2" || komut.isim === "uyarı3")) {
-  if (!message.member.roles.cache.has("645674008229969920") && !message.member.roles.cache.has(ayar.sahipRolu) && !conf.sahip.some(id => message.author.id === id)) return;
-    uye.roles.cache.has(komut.rol) ? uye.roles.remove(komut.rol) : uye.roles.add(komut.rol);
-    return message.react(client.emojiler.onay);
-  };
-  if (komut && (komut.isim === "streamer" || komut.isim === "youtuber" || komut.isim === "coder" || komut.isim === "famous")) {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    uye.roles.cache.has(komut.rol) ? uye.roles.remove(komut.rol) : uye.roles.add(komut.rol);
-    return message.react(client.emojiler.onay);
-  };
-
-  if (komut && (komut.isim === "elite")) {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    uye.roles.cache.has(komut.rol) ? uye.roles.remove(komut.rol) : uye.roles.add(komut.rol);
-    return message.react(client.emojiler.onay);
-  };
-  if (!message.member.roles.cache.has("603665487662153738") && !message.member.roles.cache.has(ayar.sahipRolu) && !conf.sahip.some(id => message.author.id === id)) return;
-  if (!uye || !komut) return;
-  uye.roles.cache.has(komut.rol) ? uye.roles.remove(komut.rol).catch() : uye.roles.add(komut.rol).catch();
-  return message.react(client.emojiler.onay);
-};
-
-module.exports.configuration = {
-  name: "message"
 };
