2bd749a1d419722ea89e09b3d0b596f51c2bfa0b
Glitch (hello-express) <none>
2021-02-16 10:02:52 +0000

    🏐🏘 Checkpoint ./LoadingEvents/Talent.js:15912651/1406 ./authorization.json:15912651/121 ./watch.json:15912651/4

--- LoadingEvents/Talent.js
+++ LoadingEvents/Talent.js
@@ -6,7 +6,7 @@ class Talent {
   }
 
   async TalentPerms() {
-    client.komutlar = ["vip", "elite"];
+    client.komutlar = ["vip", "elite", "streamer", "şair"];
     if (!Array.isArray(auth.GuildData.Prefixes)) auth.GuildData.Prefixes = [auth.GuildData.Prefixes];
     if (!auth.GuildData.Prefixes.some(x => this.msg.content.startsWith(x.toLowerCase()))) return;
     if (this.msg.author.bot || this.msg.guild.id !== auth.GuildData.GuildID || this.msg.channel.type === "dm") return;
@@ -22,7 +22,17 @@ class Talent {
       await member.roles.cache.has(auth.Perms.Vip) ? member.roles.remove(auth.Perms.Vip) : member.roles.add(auth.Perms.Vip);
       await this.msg.react(client.react("duztik")).catch(() => { });
       client.message(client.embed(`${member} adlı üyede <@&${auth.Perms.Vip}> permi için gerekli işlemler yapılmıştır.`, this.msg), this.msg.channel.id, 4500);
-    } else if ((komut) && (komut === ""))
+      return client.message(client.embed(`${author} adlı yetkili ${member} (\`${member.id}\`) adlı kullanıcıya <@&${auth.Perms.Vip}> permi için gerekli işlemler yapılmıştır.`, this.msg), auth.GuildData.Chats.AbilityLog)
+    } else if ((komut) && (komut === "streamer")) {
+      if ((!author.roles.cache.some(r => auth.Perms.TalentAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
+      if (!member) return client.message(client.noMember(this.msg), this.msg.channel.id, 6500);
+      await member.roles.cache.has(auth.Talent.Streamer) ? member.roles.remove(auth.Talent.Streamer) : member.roles.add(auth.Talent.Streamer);
+      await this.msg.react(client.react("duztik")).catch(() => { });
+      client.message(client.embed(`${member} adlı üyede <@&${auth.Talent.Streamer}> permi için gerekli işlemler yapılmıştır.`, this.msg), this.msg.channel.id, 4500);
+      return client.message(client.embed(`${author} adlı yetkili ${member} (\`${member.id}\`) adlı kullanıcıya <@&${auth.Talent.Streamer}> permi için gerekli işlemler yapılmıştır.`, this.msg), auth.GuildData.Chats.AbilityLog)
+    } else if ((komut) && (komut === "şair")) {
+      
+    }
   }
 }
 
