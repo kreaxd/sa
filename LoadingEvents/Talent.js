b4177c6a23b014e98bc1b70ab0da105d28d2e3fa
Glitch (hello-express) <none>
2021-02-16 09:50:53 +0000

    🍐🐃 Checkpoint ./LoadingEvents/Talent.js:15912651/740 ./watch.json:15912651/16

--- LoadingEvents/Talent.js
+++ LoadingEvents/Talent.js
@@ -5,28 +5,28 @@ class Talent {
     this.msg = msg;
   }
 
-  /*async TalentPerms() {
+  async TalentPerms() {
     client.komutlar = ["vip", "elite"];
     if (!Array.isArray(auth.GuildData.Prefixes)) auth.GuildData.Prefixes = [auth.GuildData.Prefixes];
     if (!auth.GuildData.Prefixes.some(x => this.msg.content.startsWith(x.toLowerCase()))) return;
     if (this.msg.author.bot || this.msg.guild.id !== auth.GuildData.GuildID || this.msg.channel.type === "dm") return;
-    let args = auth.GuildData.Prefixes.some(x => this.msg.content.substring(x.length)).split(" ");
+    let args = this.msg.content.substring(auth.GuildData.Prefixes.some(x => x.length)).split(" ");
     let command = args[0];
-    args = args.splice(1);
+    args = args.slice(1);
+    let author = this.msg.guild.member(this.msg.author);
     let member = this.msg.guild.member(this.msg.mentions.users.first()) || this.msg.guild.members.cache.get(args[0]);
     let komut = client.komutlar.find(k => k === command);
     if ((komut) && (komut === "vip" || komut === "elite")) {
-      if ((!this.msg.author.roles.cache.some(r => auth.Perms.TalentAuth.includes(r.id))) && (!this.msg.author.permissions.has("ADMINISTRATOR"))) return;
+      if ((!author.roles.cache.some(r => auth.Perms.TalentAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
       if (!member) return client.message(client.noMember(this.msg), this.msg.channel.id, 6500);
       await member.roles.cache.has(auth.Perms.Vip) ? member.roles.remove(auth.Perms.Vip) : member.roles.add(auth.Perms.Vip);
       await this.msg.react(client.react("duztik")).catch(() => { });
       client.message(client.embed(`${member} adlı üyede <@&${auth.Perms.Vip}> permi için gerekli işlemler yapılmıştır.`, this.msg), this.msg.channel.id, 4500);
-    }
+    } else if ((komut) && (komut === ""))
   }
-  /*/
 }
 
 module.exports.event = {
   name: "message",
-  eventOn: message => new Talent(message)
+  eventOn: message => new Talent(message).TalentPerms()
 };
