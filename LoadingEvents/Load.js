3d9435507a533fd06415df7e6649cf0d0d3dfbd3
Glitch (hello-express) <none>
2021-02-16 14:08:54 +0000

    🛎🌒 Checkpoint ./LoadingEvents/Load.js:15912651/80

--- LoadingEvents/Load.js
+++ LoadingEvents/Load.js
@@ -6,7 +6,7 @@ class Ready {
     this.guild = guild;
   }
   kur() {
-    client.user.setStatus("idle");
+     client.user.setActivity("I Love You Mâze");
     console.log("("+client.user.username +") adlı hesapta [" +this.guild.name+"] adlı sunucuda giriş yapıldı.");
   }
 }
