0ef27461d1d2f050a3942a0c1827dc8d8dd6382b
Glitch (tillahmoderasyon) <none>
2021-02-17 22:40:23 +0000

    ✈️🌵 Checkpoint ./LoadingCommands/Say.js:1208823/16 ./watch.json:1208823/2

--- LoadingCommands/Say.js
+++ LoadingCommands/Say.js
@@ -1,7 +1,7 @@
 module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed} = require("discord.js")) => {
  if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
     const MemberSize = msg.guild.memberCount;
-    const TaglıSize = msg.guild.members.cache.filter(u => u.user.username.includes(auth.Tags.RealTag)).size + 4352344;
+    const TaglıSize = msg.guild.members.cache.filter(u => u.user.username.includes(auth.Tags.RealTag)).size + 221;
     const OnlineSize = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
     const VoiceSize = msg.guild.channels.cache.filter(c => c.type === "voice").map(c=> c.members.size).reduce((a,b) => a+b) || 0;
       msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setThumbnail(msg.guild.iconURL({dynamic: true})).setDescription(`
