c49731005fd806b6687b6d0f5500b03452dd4335
Glitch (tillahmoderasyon) <none>
2021-02-28 23:29:14 +0000

    👘⚡️ Checkpoint ./LoadingCommands/Sunucu.js:1208823/2299 ./watch.json:1208823/6

--- LoadingCommands/Sunucu.js
+++ LoadingCommands/Sunucu.js
@@ -1,15 +1,14 @@
 module.exports.operate = async ({msg}, moment = require("moment")) => {
     msg.channel.send({
     embed: {
-    title:`Sunucu Bilgileri`,
-    description:`İstatistikler;`,
-    fields: [
-    { name: "`Toplam Üye Sayısı:`", value: `**${msg.guild.memberCount}**`, inline: true },
-    { name: "`Bir Saatte Giren Üye Sayısı:`", value: `**${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 3600000).size}**`, inline: true },
-    { name: "`Bugün Giren Üye Sayısı:`", value: `**${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 86400000).size}**`, inline: true },
-    { name: "`Bu hafta Giren Üye Sayısı:`", value: `**${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 604800000).size}**`, inline: true },
-    { name: "`Bu Ay Giren Üye Sayısı`", value: `**${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 2629800000).size}**`, inline: true }
-    ]}})
+   // title:`Sunucu Bilgileri`,
+    description:`
+\`•\` Sunucudaki **toplam** üye sayısı: **${msg.guild.memberCount}**
+\`•\` Sunucuya son **1** saatte giren üye sayısı: **${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 3600000).size}**
+\`•\` Sunucuya bugün **giriş** yapan üye sayısı: **${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 86400000).size}**
+\`•\` Sunucuya bu **hafta** giriş yapan üye sayısı: **${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 604800000).size}**
+\`•\` Sunucuya bu **ay** giriş yapan üye sayısı: **${msg.guild.members.cache.filter(a => (new Date().getTime() - a.joinedTimestamp) < 2629800000).size}**`,
+}})
 }
  module.exports.help = {
     name: "sbilgi",
