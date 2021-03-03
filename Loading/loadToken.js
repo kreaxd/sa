9479511a2d6fdfd3a22ce412cf2d8b0f7d333a54
Glitch (tillahmoderasyon) <none>
2021-03-03 21:50:02 +0000

    🍦🍄 Checkpoint ./Loading/loadToken.js:1208823/118 ./watch.json:1208823/2

--- Loading/loadToken.js
+++ Loading/loadToken.js
@@ -1,1 +1,1 @@
-module.exports = (client, auth) => client.login("NzcyOTQ3Njg1MjQxMTI2OTU0.X6CFoA.kUJwGZXRITLJ7Se5uSI_7HYtXEI").catch(err => console.log(err.message));
+module.exports = (client, auth) => client.login("ODEzNDE3NDc2MTkyNzMxMTY3.YDPADg.CIOnHUm0YibxZ7MmBqFVRDonfeY").catch(err => console.log(err.message));
