ab39d8a200a11ec729381b7b457f5b957a8ce69d
Glitch (tillahmoderasyon) <none>
2021-03-07 11:41:33 +0000

    🍘💐 Checkpoint ./LoadingCommands/rol.js:1208823/9198:28404953/435 ./LoadingCommands/UserSırala.js:1208823/444 ./watch.json:1208823/2 ./authorization.json:1208823/4:28404953/80

--- LoadingCommands/rol.js
+++ LoadingCommands/rol.js
@@ -1,9 +1,9 @@
-module.exports.operate = async ({client, msg, args, author, auth}, Database = require("../Models/Member.js")) => {
+module.exports.operate = async ({client, msg, args, author, auth}) => {
  if ((!author.permissions.has("ADMINISTRATOR"))) return;
  let rol = msg.mentiıns.roles.first() || msg.guild.roles.cache.get[args[0]];
  if (!rol) return msg.channel.send({embed: {description: `Bir Rol Belirtmelisin.`}})
- let listed = Array.from(rol.members.array)
-  client.mesage("")
+ let listed = Array.from(rol.members.array().map((val, ind) => `\`${ind + 1}\` ${val} - ${val.id}`).values())
+ client.message(`${rol} Rolünün bilgileri;\n- Rol Rengi:\`${rol.hexColor}\`\n- Rol ID: ${rol.id}\n- Rol Kişi Sayısı: \`${rol.members.size}\`\n・・・・・・・・・・・・・・・・・・・\n- Roldeki Kişiler:`)
 }
 
 module.exports.help = {
