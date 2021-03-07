77fd30c4a2ce87f448363bff5f2d48978ee81053
Glitch (tillahmoderasyon) <none>
2021-03-07 17:42:45 +0000

    💠🐯 Checkpoint ./watch.json:1208823/8:28404953/2 ./authorization.json:28404953/18 ./LoadingCommands/ekip.js:1208823/1020:28404953/4800

--- LoadingCommands/ekip.js
+++ LoadingCommands/ekip.js
@@ -1,35 +1,15 @@
 module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed} = require("discord.js")) => {
   if ((!author.permissions.has("ADMINISTRATOR"))) return;
-    const ekip1 = msg.guild.members.cache.filter(x => x.roles.cache.has(auth.Ekip.Ekip1)).size;
-    const ekip2 = msg.guild.members.cache.filter(x => x.roles.cache.has(auth.Ekip.Ekip2)).size;
-    const ekip3 = msg.guild.members.cache.filter(x => x.roles.cache.has(auth.Ekip.Ekip3)).size;
-    const ekip4 = msg.guild.members.cache.filter(x => x.roles.cache.has(auth.Ekip.Ekip4)).size;
-    const ekip5 = msg.guild.members.cache.filter(x => x.roles.cache.has(auth.Ekip.Ekip5)).size;
-  
-    const ekip1ses = msg.guild.channels.cache.filter(c => c.type === "voice").map(k => {ekip1ses += k.members.size}).includes(auth.Ekip.Ekip1).size;
-    const ekip2ses = msg.guild.channels.cache.filter(c => c.type === "voice").map(k => {ekip2ses += k.members.size}).includes(auth.Ekip.Ekip1).size;
-    const ekip3ses = msg.guild.channels.cache.filter(c => c.type === "voice").map(k => {ekip3ses += k.members.size}).includes(auth.Ekip.Ekip1).size;
-    const ekip4ses = msg.guild.channels.cache.filter(c => c.type === "voice").map(k => {ekip4ses += k.members.size}).includes(auth.Ekip.Ekip1).size;
-    const ekip5ses = msg.guild.channels.cache.filter(c => c.type === "voice").map(k => {ekip5ses += k.members.size}).includes(auth.Ekip.Ekip1).size;
-  
-    let ekip1onlineses = msg.guild.members.cache.filter(u => { return (u.roles.cache.some(r => auth.Ekip.Ekip1.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline")});
-    let ekip2onlineses = msg.guild.members.cache.filter(u => { return (u.roles.cache.some(r => auth.Ekip.Ekip2.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline")});
-    let ekip3onlineses = msg.guild.members.cache.filter(u => { return (u.roles.cache.some(r => auth.Ekip.Ekip3.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline")});
-    let ekip4onlineses = msg.guild.members.cache.filter(u => { return (u.roles.cache.some(r => auth.Ekip.Ekip4.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline")});
-    let ekip5onlineses = msg.guild.members.cache.filter(u => { return (u.roles.cache.some(r => auth.Ekip.Ekip5.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline")});
-  
-    const ekip1online = msg.guild.members.cache.filter(u => u.presence.status !== "offline").includes(auth.Ekip.Ekip1).size;
-    const ekip2online = msg.guild.members.cache.filter(u => u.presence.status !== "offline").includes(auth.Ekip.Ekip2).size;
-    const ekip3online = msg.guild.members.cache.filter(u => u.presence.status !== "offline").includes(auth.Ekip.Ekip3).size;
-    const ekip4online = msg.guild.members.cache.filter(u => u.presence.status !== "offline").includes(auth.Ekip.Ekip4).size;
-    const ekip5online = msg.guild.members.cache.filter(u => u.presence.status !== "offline").includes(auth.Ekip.Ekip5).size;
-    const VoiceSize = msg.guild.channels.cache.filter(c => c.type === "voice").map(c=> c.members.size).reduce((a,b) => a+b) || 0;
-        if (!args[1]) {
-        msg.channel.send({embed: {title: `Ekip Bilgilendirme;`,description: `Ekip Komutları:\n\n\`.ekip 1\` : ${auth.Ekip.Ekip1}\n\`.ekip 2\` : ${auth.Ekip.Ekip2}\n \`.ekip 3\` : ${auth.Ekip.Ekip3}\n \`.ekip 4\` : ${auth.Ekip.Ekip4}\n \`.ekip 5\` : ${auth.Ekip.Ekip5}`}})
-    } else if (["ekip1"].includes(args[1])) {
+    const ekip1 = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip1.Ekip.includes(x.id)))).size || 0;
+    const ekip1tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip1.includes(x.id))) && (u.user.username.includes(auth.Tags.RealTag))).size || 0;
+    const ekip1onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip1.includes(x.id))) && (u.presence.status !== "offline")).size;
+    const ekip1ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip1.includes(x.id))) && (c.voice.channel)).size || 0;
+  if (!args[1]) {
+        msg.channel.send({embed: {title: `Ekip Bilgilendirme;`,description: `Ekip Komutları:\n\n\`.ekip 1\`: <@&${auth.Ekip.Ekip1}>\n\`.ekip 2\`: <@&${auth.Ekip.Ekip2}>\n \`.ekip 3\`: <@&${auth.Ekip.Ekip3}>\n \`.ekip 4\`: <@&${auth.Ekip.Ekip4}>\n \`.ekip 5\`: <@&${auth.Ekip.Ekip5}>`}})
+    } else if (["ses"].includes(args[1])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setTitle(`${auth.Ekip.Ekip1} Bilgileri`).setDescription(`
 \`>\` Ekipte toplam **${ekip1}** üye var.
-\`>\` Ekipte toplam **${ekip1online}** çevrimiçi üye var.
+\`>\` Ekipte toplam **${ekip1tag}** taglı üye var.
 \`>\` Seste toplam **${ekip1ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip1onlineses}** kullanıcı var.
 `))
