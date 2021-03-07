10b4a960ed08a55ef7259bfa08f0ef4ab1e8e147
Glitch (tillahmoderasyon) <none>
2021-03-07 12:50:09 +0000

    🍠🚓 Checkpoint ./LoadingCommands/YtKapat.js:28404953/554

--- LoadingCommands/YtKapat.js
+++ LoadingCommands/YtKapat.js
@@ -1,16 +1,11 @@
 module.exports.operate = async ({client, msg, member ,args, auth, author}) => { 
-if (!auth.GuildData.YtAc.includes(author.id)) return null;
-msg.react(client.react("duztik"));
-if(["ac"].includes[args[0]]) { 
-msg.guild.roles.cache.get(auth.ytac.ytperm).setPermissions(8)
-msg.channel.send({embed: {description:`Yönetici yetkisi ${auth.ytac.ytperm} permi için açılmışır.`}})
-} if(["kapat"].includes[args[0]]) {
-msg.guild.roles.cache.get(auth.ytac.ytperm).setPermissions(0)
-msg.channel.send({embed: {description:`Yönetici yetkisi ${auth.ytac.ytperm} permi için kapatılmıştır.`}})
-}
+      if (!auth.GuildData.YtAc.includes(author.id)) return null;
+        msg.guild.roles.cache.get(auth.ytac.ytperm).setPermissions(0)
+    msg.react(client.react("duztik"));
+ msg.channel.send({embed: {description:`Yönetici yetkisi ${auth.ytac.ytperm} permi için kapanmıştır.`}})
 };
 
 module.exports.help = {
-  name: "yt",
+  name: "ytkapat",
   alias: []
 }; 
