8b71f78bbfb24ffe350ecfaef42c12cf6c967291
Glitch (tillahmoderasyon) <none>
2021-03-13 10:14:34 +0000

    🐛🚇 Checkpoint ./LoadingCommands/KullaniciBilgi.js:1208823/775 ./watch.json:1208823/2

--- LoadingCommands/KullaniciBilgi.js
+++ LoadingCommands/KullaniciBilgi.js
@@ -13,6 +13,18 @@ module.exports.operate = async ({client, msg, author, member}, Discord = require
       } else {
         sa = slm[Object.keys(mention.presence.clientStatus)[0]];
       } 
+  let flags = msg.author.flags.toArray()
+  let uflags = flags.map(x => x.toString()).join(",");
+   uflags = uflags.replace("HOUSE_BRAVERY", ":rozet_bravery: HypeSquad Bravery")
+   uflags = uflags.replace("HOUSE_BALANCE",":rozet_balance:  HypeSquad Balance")
+   uflags = uflags.replace("DISCORD_PARTNER",":rozet_partner: Partner")
+   uflags = uflags.replace("HYPESQUAD_EVENTS",":rozet_hypesquad: Hypesquad Event")
+   uflags = uflags.replace("EARLY_SUPPORTER",":rozet_early: Early Supporter")
+   uflags = uflags.replace("VERIFIED_DEVELOPER",":rozet_developer: Verified Bot Developer")
+  uflags = uflags.replace("HOUSE_BRILLIANCE",":rozet_brillance: HypeSquad Brilliance")
+  if(flags == ""){
+    uflags = "Rozet bulunamadı"
+  }
 const xd = new Discord.MessageEmbed()
 .setAuthor(`${mention.tag}`, mention.avatarURL({ dynamic: true}))
 .setThumbnail(mention.avatarURL({ dynamic: true}))
@@ -21,7 +33,7 @@ const xd = new Discord.MessageEmbed()
 \`Profil\` **•** ${mention} ${mention.presence.status.replace('online', 'Çevrim İçi').replace('idle', 'Boşta').replace('dnd', 'Rahatsız Etmeyin').replace('offline', 'Çevrim Dışı')}
 \`Bağlandığı yer\` **•** ${sa}
 \`Kuruluş Tarihi\` **•** ${moment(mention.createdAt).format('D/MMMM/YYYY')}
-
+${flags}
 **• Kullanıcı bilgisi**
 \`Takma Ad\` **•** **${mentionMember.displayName}**
 \`Sunucuya Giriş Tarihi\` **•** ${moment(mentionMember.joinedAt).format('D/MMMM/YYYY')}
