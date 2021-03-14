69d341e74d658efd23769932a98025a29109eb57
Glitch (tillahmoderasyon) <none>
2021-03-14 08:47:26 +0000

    👾🗻 Checkpoint ./LoadingCommands/Say.js:1208823/8618 ./watch.json:1208823/4

--- LoadingCommands/Say.js
+++ LoadingCommands/Say.js
@@ -4,11 +4,14 @@ module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed
     const TaglıSize = msg.guild.members.cache.filter(u => u.user.username.includes(auth.Tags.RealTag)).size;
     const OnlineSize = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
     const VoiceSize = msg.guild.channels.cache.filter(c => c.type === "voice").map(c=> c.members.size).reduce((a,b) => a+b) || 0;
+    const boost = msg.guild.premiumSubscriptionCount
+    const boostlevel = msg.guild.premiumTier
       msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
-\`>\` Sunucumuzda toplam **${MemberSize}** üye var.
-\`>\` Sunucumuzda toplam **${OnlineSize}** çevrimiçi üye var.
-\`>\` Toplam **${TaglıSize}** kişi tagımıza sahip.
-\`>\` Seste toplam **${VoiceSize}** kullanıcı var.`))
+\`•\` Sunucumuzda toplam **${MemberSize}** üye var.
+\`•\` Sunucumuzda toplam **${OnlineSize}** çevrimiçi üye var.
+\`•\` Toplam **${TaglıSize}** kişi tagımıza sahip.
+\`•\` Sunucumuzda toplam **${boost}** boost bulunmakta. **(${boostlevel}. seviye)**
+\`•\` Seste toplam **${VoiceSize}** kullanıcı var.`))
     msg.react(client.react("duztik")).catch(() => { }); // .setThumbnail(msg.guild.iconURL({dynamic: true}))
   };
   
