f01075577f84ae2c80f52fe32dd597a1b0bd2439
Glitch (tillahmoderasyon) <none>
2021-03-26 18:26:15 +0000

    👷⏲ Checkpoint ./LoadingCommands/Say.js:25076867/610 ./watch.json:25076867/2

--- LoadingCommands/Say.js
+++ LoadingCommands/Say.js
@@ -2,6 +2,7 @@ module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed
  if ((!author.roles.cache.some(r => auth.Perms.RegisterAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
     const etiket =  msg.guild.members.cache.filter(s => !s.bot).filter(member => member.user.discriminator == "1485").size;
     const etiket2 =  msg.guild.members.cache.filter(s => !s.bot).filter(member => member.user.discriminator == "0440").size;
+    const etiket3 =  msg.guild.members.cache.filter(s => !s.bot).filter(member => member.user.discriminator == "0606").size;
     const MemberSize = msg.guild.memberCount;
     const TaglıSize = msg.guild.members.cache.filter(u => u.user.username.includes(auth.Tags.RealTag)).size;
     const TaglıSize2 = msg.guild.members.cache.filter(u => u.user.username.includes("Réwind")).size;
@@ -11,6 +12,8 @@ module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed
     const TaglıSize6 = msg.guild.members.cache.filter(u => u.user.username.includes("réwind")).size;
     const TaglıSize7 = msg.guild.members.cache.filter(u => u.user.username.includes("réwînd")).size;
     const TaglıSize8 = msg.guild.members.cache.filter(u => u.user.username.includes("Vortex")).size; 
+    const TaglıSize9 = msg.guild.members.cache.filter(u => u.user.username.includes("Wayne")).size; 
+    const TaglıSize10 = msg.guild.members.cache.filter(u => u.user.username.includes("Wâyne")).size; 
     const OnlineSize = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
     const VoiceSize = msg.guild.channels.cache.filter(c => c.type === "voice").map(c=> c.members.size).reduce((a,b) => a+b) || 0;
     const boost = msg.guild.premiumSubscriptionCount
@@ -18,8 +21,8 @@ module.exports.operate = async ({client, msg, args, author, auth}, {MessageEmbed
       msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
 \`•\` Sunucumuzda toplam **${MemberSize}** üye var.
 \`•\` Sunucumuzda toplam **${OnlineSize}** çevrimiçi üye var.
-\`•\` Toplam \`Rewind:\` **${TaglıSize}** - \`Réwind:\` **${TaglıSize2}** - \`Rèwînd:\` **${TaglıSize3}** - \`Rewînd:\` **${TaglıSize4}** - \`rewind:\` **${TaglıSize5}** - \`réwind:\` **${TaglıSize6}** - \`réwînd:\` **${TaglıSize7}** - Vortex **${TaglıSize8}**
-\`•\` Toplam **${etiket}** \`(#1485)\` ve **${etiket2} \`(Vortex)\` etiketlerimizi alan taglı üyelerimiz var.
+\`•\` Toplam \`Rewind:\` **${TaglıSize}** - \`Réwind:\` **${TaglıSize2}** - \`Rèwînd:\` **${TaglıSize3}** - \`Rewînd:\` **${TaglıSize4}** - \`rewind:\` **${TaglıSize5}** - \`réwind:\` **${TaglıSize6}** - \`réwînd:\` **${TaglıSize7}** - \`Vortex\` **${TaglıSize8}** - \`Wayne\` **${TaglıSize9}** - \`Wâyne\` **${TaglıSize10}**
+\`•\` Toplam **${etiket}** \`(#1485)\`, **${etiket2}** \`(#0440)\` ve **${etiket3}** \`(#0606)\` etiketlerimizi alan taglı üyelerimiz var.
 \`•\` Sunucumuzda toplam **${boost}** boost bulunmakta. **(${boostlevel}. seviye)**
 \`•\` Seste toplam **${VoiceSize}** kullanıcı var.`))
     msg.react(client.react("duztik")).catch(() => { }); // .setThumbnail(msg.guild.iconURL({dynamic: true}))
