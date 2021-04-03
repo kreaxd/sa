f2ae78706eaf59d258bab4f2980971696630731c
Glitch (tillahmoderasyon) <none>
2021-04-03 19:48:24 +0000

    🍖🐵 Checkpoint ./Models/Snipe.js :25076867/1802 ./LoadingCommands/Snipee.js:25076867/846 ./watch.json:25076867/10 ./Models/Snipe.js:25076867/18

--- LoadingCommands/Snipee.js
+++ LoadingCommands/Snipee.js
@@ -1,14 +1,9 @@
-const snipe = require("../schemas/snipe");
+const snipe = require("../Models/Snipe.js");
 const moment = require("moment");
 require("moment-duration-format");
 const { MessageEmbed } = require("discord.js");
 
-module.exports.operate = async ({client, msg, args, auth, author}) => { 
-  if (!author.hasPermission("MANAGE_ROLES")) return;
-    msg.react(client.react("duztik"));
-  msg.channel.send({embed: {title: `En son silinen mesaj bilgisi:`, description: `Silen Kişi: <@${client.snipe[msg.channel.id].atan}>\n Silinen Mesaj: \`${client.snipe[msg.channel.id].content}\`\nSilindiği Zaman: \`${client.snipe[msg.channel.id].tarih} \``, author: {name: msg.guild.name, icon_url: msg.guild.iconURL({dynamic:true})}, color:client.renk[Math.floor(Math.random()*client.renk.length)]}});
-    client.channels.cache.get(auth.Logs.KomutLog).send(`⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n${auth.Reacts.star} ${msg.author.tag}(\`${msg.author.id}\`) kullanıcısı <#${msg.channel.id}> kanalında bir komut kullandı.\n**Komutun içeriği:** \`${msg.content}\``)
-};
+module.exports.operate = async ({client, message, args, auth}) => { 
     const data = await snipe.findOne({ guildID: message.guild.id, channelID: message.channel.id });
     if (!data) return message.channel.error(message, "Bu kanalda silinmiş bir mesaj bulunmuyor!");
     const author = await client.fetchUser(data.author);
@@ -19,7 +14,6 @@ module.exports.operate = async ({client, msg, args, auth, author}) => {
     if (author) embed.setAuthor(author.tag, author.avatarURL({ dynamic: true, size: 2048 }));
     if (data.image) embed.setImage(data.image);
     message.channel.send(embed);
-  },
 };
 module.exports.help = {
   name: "snipee",
