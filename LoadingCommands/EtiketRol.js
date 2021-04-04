49708659f0a421b64fd49d580cb8d59f3188f489
Glitch (tillahmoderasyon) <none>
2021-04-04 08:50:33 +0000

    🏬🌍 Checkpoint ./LoadingCommands/EtiketRol.js:25076867/198 ./watch.json:25076867/6

--- LoadingCommands/EtiketRol.js
+++ LoadingCommands/EtiketRol.js
@@ -1,8 +1,8 @@
 module.exports.operate = async ({client, msg, args, member, auth, author}, ms = require("ms"), Discord = require("discord.js"), Database = require("../Models/Restriction.js"), Schema = require("../Models/Member.js")) => {
   if ((!author.roles.cache.some(r => auth.Perms.MuteAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
-    let rol = "811045009184522281"
-    let tag = "stâr"
-    let etiket = "7730" 
+    let tag = args[0];
+    let etiket = args[1]; 
+    let rol = args[2];
     msg.guild.members.cache.filter(s => s.user.discriminator === etiket || s.user.username.includes(tag) && !s.roles.cache.has(rol)).forEach(m => m.roles.add(rol))
     msg.channel.send(`
 Kullanıcı adında \`${tag}\` ve etiketinde \`#${etiket}\` bulunduran kullanıcılara rol veriliyor.
