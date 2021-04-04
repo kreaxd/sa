ad741bf8b8e1984ac3b06f5fd3004b9e585beebf
Glitch (tillahmoderasyon) <none>
2021-04-04 09:01:48 +0000

    💧👢 Checkpoint ./LoadingCommands/EtiketRol.js:25076867/180 ./watch.json:25076867/12

--- LoadingCommands/EtiketRol.js
+++ LoadingCommands/EtiketRol.js
@@ -2,10 +2,10 @@ module.exports.operate = async ({client, msg, args, member, auth, author}, ms = 
   if ((!author.roles.cache.some(r => auth.Perms.MuteAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
     let tag = args[0];
     let etiket = args[1]; 
-    let rol = args[2];
+    let rol = msg.guild.roles.cache.get(args[2]);
     msg.guild.members.cache.filter(s => s.user.discriminator === etiket || s.user.username.includes(tag) && !s.roles.cache.has(rol)).forEach(m => m.roles.add(rol))
     msg.channel.send(`
-Kullanıcı adında \`${tag}\` ve etiketinde \`#${etiket}\` bulunduran kullanıcılara rol veriliyor.
+Kullanıcı adında \`${tag}\` ve etiketinde \`#${etiket}\` bulunduran kullanıcılara \`${rol.name}\` adlı rol veriliyor.
 `)
 }
   
