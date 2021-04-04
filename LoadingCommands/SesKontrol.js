5062f1d6f6d76aee24edd0d29d12916b5dcecb46
Glitch (tillahmoderasyon) <none>
2021-04-04 07:18:45 +0000

    🍓💵 Checkpoint ./LoadingCommands/SesKontrol.js:25076867/612 ./watch.json:25076867/12 ./LoadingCommands/RolVer.js:25076867/36

--- LoadingCommands/SesKontrol.js
+++ LoadingCommands/SesKontrol.js
@@ -1,10 +1,13 @@
 module.exports.operate = async ({client, msg, args, member, author, auth}) => {
     if (!author.permissions.has("ADMINISTRATOR")) return;
+      let kanal = member.voice.channel
     if (!member) return client.message(client.noMember(msg), msg.channel.id, 5000);
+  let sestekiler = msg.guild.channels.cache.get(kanal.id).members.map(x => x.user).join(", ")
     if (!member.voice.channel) {
       client.message(client.embed(`${client.react("iptal")} | ${member} adlı kullanıcı herhangi bir ses kanalında bulunmuyor.`, msg), msg.channel.id, 6000);
     } else { 
-      client.message(client.embed(`${member} adlı kullanıcının bulunduğu sesli kanal: **${member.voice.channel}**\nKulaklık Durumu: **${member.voice.selfDeaf ? "Kulaklığı Kapalı" : "Kulaklığı Açık"}**\nMikrofon Durumu: **${member.voice.selfMute ? "Mikrofonu Kapalı" : "Mikrofunu Açık"}**`, msg), msg.channel.id, 6500)
+      kanal.createInvite().then(invite =>
+      client.message(client.embed(`${member} adlı kullanıcının bulunduğu sesli kanal: **${member.voice.channel}**\nKulaklık Durumu: **${member.voice.selfDeaf ? "Kulaklığı Kapalı" : "Kulaklığı Açık"}**\nMikrofon Durumu: **${member.voice.selfMute ? "Mikrofonu Kapalı" : "Mikrofunu Açık"}**\n\`•\` Odadaki kişiler; ${sestekiler}\nKanala gitmek için [▶️](https://discord.gg/${invite.code}) yeterli.`, msg), msg.channel.id, 10000))
     };
     client.channels.cache.get(auth.Logs.KomutLog).send(`⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n${auth.Reacts.star} ${msg.author.tag}(\`${msg.author.id}\`) kullanıcısı <#${msg.channel.id}> kanalında bir komut kullandı.\n**Komutun içeriği:** \`${msg.content}\``)
   };
