5d4182a3ab33d4683f1f49952e3c3f02dd4e4386
Glitch (tillahmoderasyon) <none>
2021-04-10 23:56:02 +0000

    🚍🍿 Checkpoint ./LoadingCommands/ekip.js:1901910/47 ./watch.json:1901910/8 ./LoadingCommands/Ekip.js:1901910/1845 ./authorization.json:1901910/43

--- LoadingCommands/Ekip.js
+++ LoadingCommands/Ekip.js
@@ -25,18 +25,32 @@ const ekip5tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => au
 const ekip5ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (c.voice.channel)).size || 0;
 const ekip5onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (u.presence.status !== "offline")).size;
   
-const ekip6 = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id)))).size || 0;
-const ekip6tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (u.user.username.includes(auth.Tags.RealTag))).size || 0;
-const ekip6ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (c.voice.channel)).size || 0;
-const ekip6onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (u.presence.status !== "offline")).size;
+const ekip6 = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip6.includes(x.id)))).size || 0;
+const ekip6tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip6.includes(x.id))) && (u.user.username.includes(auth.Tags.RealTag))).size || 0;
+const ekip6ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip6.includes(x.id))) && (c.voice.channel)).size || 0;
+const ekip6onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip6.includes(x.id))) && (u.presence.status !== "offline")).size;
   
-const ekip7 = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id)))).size || 0;
-const ekip7tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (u.user.username.includes(auth.Tags.RealTag))).size || 0;
-const ekip7ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (c.voice.channel)).size || 0;
-const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip5.includes(x.id))) && (u.presence.status !== "offline")).size;
+const ekip7 = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip7.includes(x.id)))).size || 0;
+const ekip7tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip7.includes(x.id))) && (u.user.username.includes(auth.Tags.RealTag))).size || 0;
+const ekip7ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip7.includes(x.id))) && (c.voice.channel)).size || 0;
+const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip7.includes(x.id))) && (u.presence.status !== "offline")).size;
+  
+const ekip8 = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip8.includes(x.id)))).size || 0;
+const ekip8tag = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip8.includes(x.id))) && (u.user.username.includes(auth.Tags.RealTag))).size || 0;
+const ekip8ses = msg.guild.members.cache.filter(c => (c.roles.cache.some(x => auth.Ekip.Ekip8.includes(x.id))) && (c.voice.channel)).size || 0;
+const ekip8onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x => auth.Ekip.Ekip8.includes(x.id))) && (u.presence.status !== "offline")).size;
   if (!args[0]) {
-        msg.channel.send({embed: {footer: `Tüm Ekip Bilgilerine Bakmak İçin .ekip tüm yazabilirsiniz.`,title: `Ekip Bilgilendirme;`,description: `Ekip Komutları:\n\n\`.ekip 1\`: <@&${auth.Ekip.Ekip1}>\n\`.ekip 2\`: <@&${auth.Ekip.Ekip2}>\n \`.ekip 3\`: <@&${auth.Ekip.Ekip3}>\n \`.ekip 4\`: <@&${auth.Ekip.Ekip4}>\n \`.ekip 5\`: <@&${auth.Ekip.Ekip5}>\n \`.ekip 6\`: <@&${auth.Ekip.Ekip6}>\n\`.ekip 7\`: <@&${auth.Ekip.Ekip7}>`}})
-    } else if (["1"].includes(args[0])) {
+        msg.channel.send({embed: {footer: `Tüm Ekip Bilgilerine Bakmak İçin .ekip tüm yazabilirsiniz.`,title: `Ekip Bilgilendirme;`,description: `Ekip Komutları:
+        
+        \`.ekip star\`: <@&${auth.Ekip.Ekip1}>
+        \`.ekip rose\`: <@&${auth.Ekip.Ekip2}>
+        \`.ekip hate\`: <@&${auth.Ekip.Ekip3}>
+        \`.ekip fuerte\`: <@&${auth.Ekip.Ekip4}>
+        \`.ekip always\`: <@&${auth.Ekip.Ekip5}>
+        \`.ekip vensai\`: <@&${auth.Ekip.Ekip6}>
+        \`.ekip 0004\`: <@&${auth.Ekip.Ekip7}>
+        \`.ekip Roy\`: <@&${auth.Ekip.Ekip8}>`}})
+    } else if (["star"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
 **<@&${auth.Ekip.Ekip1}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip1}** üye var.
@@ -44,7 +58,7 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Seste toplam **${ekip1ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip1onlineses}** kullanıcı var.
 `))
-    } else if (["2"].includes(args[0])) {
+    } else if (["rose"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
   **<@&${auth.Ekip.Ekip2}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip2}** üye var.
@@ -52,7 +66,7 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Seste toplam **${ekip2ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip2onlineses}** kullanıcı var.
 `))
-    } else if (["3"].includes(args[0])) {
+    } else if (["hate"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
   **<@&${auth.Ekip.Ekip3}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip3}** üye var.
@@ -60,7 +74,7 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Seste toplam **${ekip3ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip3onlineses}** kullanıcı var.
 `))
-    } else if (["4"].includes(args[0])) {
+    } else if (["fuerte"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
   **<@&${auth.Ekip.Ekip4}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip4}** üye var.
@@ -68,7 +82,7 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Seste toplam **${ekip4ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip4onlineses}** kullanıcı var.
 `))
-   } else if (["5"].includes(args[0])) {
+   } else if (["always"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
   **<@&${auth.Ekip.Ekip5}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip5}** üye var.
@@ -76,7 +90,7 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Seste toplam **${ekip5ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip5onlineses}** kullanıcı var.
 `))
-  } else if (["6"].includes(args[0])) {
+  } else if (["vensai"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
   **<@&${auth.Ekip.Ekip6}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip6}** üye var.
@@ -84,13 +98,21 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Seste toplam **${ekip6ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip6onlineses}** kullanıcı var.
 `))
-  } else if (["7"].includes(args[0])) {
+  } else if (["0004"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
   **<@&${auth.Ekip.Ekip7}>** Bilgileri;
 \`>\` Ekipte toplam **${ekip7}** üye var.
 \`>\` Ekipte toplam sunucu tagını almış **${ekip7tag}** üye var.
 \`>\` Seste toplam **${ekip7ses}** kullanıcı var.
 \`>\` Aktif olup seste olmayan **${ekip7onlineses}** kullanıcı var.
+`))
+      } else if (["roy"].includes(args[0])) {
+  msg.channel.send(new MessageEmbed().setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
+  **<@&${auth.Ekip.Ekip8}>** Bilgileri;
+\`>\` Ekipte toplam **${ekip8}** üye var.
+\`>\` Ekipte toplam sunucu tagını almış **${ekip8tag}** üye var.
+\`>\` Seste toplam **${ekip8ses}** kullanıcı var.
+\`>\` Aktif olup seste olmayan **${ekip8onlineses}** kullanıcı var.
 `))
      } else if (["tüm"].includes(args[0])) {
   msg.channel.send(new MessageEmbed().setAuthor(msg.guild.name , msg.guild.iconURL ({dynamic: true})).setColor(client.renk[Math.floor(Math.random() * client.renk.length)]).setDescription(`
@@ -128,7 +150,19 @@ const ekip7onlineses = msg.guild.members.cache.filter(u => (u.roles.cache.some(x
 \`>\` Ekipte toplam **${ekip6}** üye var.
 \`>\` Ekipte toplam sunucu tagını almış **${ekip6tag}** üye var.
 \`>\` Seste toplam **${ekip6ses}** kullanıcı var.
-\`>\` Aktif olup seste olmayan **${ekip5onlineses}** kullanıcı var.
+\`>\` Aktif olup seste olmayan **${ekip6onlineses}** kullanıcı var.
+
+**<@&${auth.Ekip.Ekip7}>** Bilgileri;
+\`>\` Ekipte toplam **${ekip7}** üye var.
+\`>\` Ekipte toplam sunucu tagını almış **${ekip7tag}** üye var.
+\`>\` Seste toplam **${ekip7ses}** kullanıcı var.
+\`>\` Aktif olup seste olmayan **${ekip7onlineses}** kullanıcı var.
+
+**<@&${auth.Ekip.Ekip8}>** Bilgileri;
+\`>\` Ekipte toplam **${ekip8}** üye var.
+\`>\` Ekipte toplam sunucu tagını almış **${ekip8tag}** üye var.
+\`>\` Seste toplam **${ekip8ses}** kullanıcı var.
+\`>\` Aktif olup seste olmayan **${ekip8onlineses}** kullanıcı var.
 `))
     };  
     client.channels.cache.get(auth.Logs.KomutLog).send(`⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n${auth.Reacts.star} ${msg.author.tag}(\`${msg.author.id}\`) kullanıcısı <#${msg.channel.id}> kanalında bir komut kullandı.\n**Komutun içeriği:** \`${msg.content}\``)
