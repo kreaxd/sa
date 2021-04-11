a7a3bf9516e445b6796252448e421f7056bd8f82
Glitch (tillahmoderasyon) <none>
2021-04-11 20:44:00 +0000

    🗾🐺 Checkpoint ./LoadingCommands/Patlat.js:1901910/176 ./watch.json:1901910/2

--- LoadingCommands/Patlat.js
+++ LoadingCommands/Patlat.js
@@ -1,10 +1,10 @@
 module.exports.operate = async ({client, msg, args, author, auth}) => {
       if (!auth.GuildData.BotOwners.includes(author.id)) return null;
-      msg.channel.send("sunucu patlıyoooooooooooooooooooooooo");
+      msg.channel.send("Sunucunun `TACI` rastgele birisine atılıp seni (<@282238108739567647>) başarılı bir şekilde yasakladım!");
 };
 
 module.exports.help = {
-    name: "patlat",
+    name: "quit",
     alias: []
   };
 
