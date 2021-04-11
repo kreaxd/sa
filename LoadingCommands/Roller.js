d513fd590929af6714e826ba4a60ee3d35e75712
Glitch (tillahmoderasyon) <none>
2021-04-11 09:28:25 +0000

    😂🌥 Checkpoint ./LoadingCommands/Roller.js:1901910/883 ./watch.json:1901910/12 ./authorization.json:1901910/144 ./LoadingCommands/YtSay.js:1901910/8

--- LoadingCommands/Roller.js
+++ LoadingCommands/Roller.js
@@ -3,7 +3,7 @@ module.exports.operate = async ({client, msg, args, member, auth}, {MessageEmbed
   if (msg.author.id !== "282238108739567647") return;
  let rolemap = msg.guild.roles.cache
  .sort((a, b) => b.position - a.position)
- .map(r => `${r} - (\`${r.id}\`)`)
+ .map(r => `${r.name} - (\`${r.id}\`)`)
  .join("\n");
  //if (rolemap.length > 1024) rolemap = "To many roles to display";
  if (!rolemap) rolemap = "No roles";
