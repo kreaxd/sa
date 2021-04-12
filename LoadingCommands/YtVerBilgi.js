6aac4d58de26ac9c1be9229b749b56e16832c844
Glitch (tillahmoderasyon) <none>
2021-04-12 16:17:41 +0000

    🗾🍦 Checkpoint ./authorization.json:1901910/82 ./watch.json:1901910/22 ./LoadingCommands/YtVerBilgi.js:1901910/1080 ./package.json:1901910/2

--- LoadingCommands/YtVerBilgi.js
+++ LoadingCommands/YtVerBilgi.js
@@ -4,14 +4,22 @@ msg.channel.send({embed: {
 color: client.renk[Math.floor(Math.random() * client.renk.length)],
 author: { name: msg.member.user.tag, icon_url:  msg.member.user.displayAvatarURL({dynamic:true}) }, 
 description: `**Rol Yardım Menüsü**
-Öncelikle bir rol vermek istiyorsanız <@&${auth.YetkiliAlım.Yetkiler[0]}> rolüne sahip olmanız gerekiyor. Bu komut sayesinde aşağıdaki rolleri kullanıcılara verebilirsiniz.
+Öncelikle bir rol vermek istiyorsanız <@&${auth.YetkiliAlım.YtAlimDm}> rolüne sahip olmanız gerekiyor. Bu komut sayesinde aşağıdaki rolleri kullanıcılara verebilirsiniz.
 ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
 
-<@&${auth.YetkiliAlım.BaslangicYetki}>: \`.rise [@Kullanıcı]\` (<@&${auth.YetkiliAlım.AltYetki}>)
-<@&${auth.YetkiliAlım.BaslangicYetki2}>: \`.moon [@Kullanıcı]\` (<@&${auth.YetkiliAlım.AltYetki}>)
-<@&${auth.YetkiliAlım.BaslangicYetki3}>: \`.shade [@Kullanıcı]\` (<@&${auth.YetkiliAlım.AltYetki}>)
-<@&${auth.YetkiliAlım.BaslangicYetki4}>: \`.dreid [@Kullanıcı]\` (<@&${auth.YetkiliAlım.AltYetki}>)
-<@&${auth.YetkiliAlım.BaslangicYetki5}>: \`.shadow [@Kullanıcı]\` (<@&${auth.YetkiliAlım.AltYetki}> or (<@&${auth.YetkiliAlım.BotCommands}>))
+\`.rise [@Kullanıcı]\` (<@&${auth.YetkiliAlım.BaslangicYetki}> - <@&${auth.YetkiliAlım.AltYetki}>)
+\`.moon [@Kullanıcı]\` (<@&${auth.YetkiliAlım.BaslangicYetki2}> - <@&${auth.YetkiliAlım.AltYetki}>)
+\`.shade [@Kullanıcı]\` (<@&${auth.YetkiliAlım.BaslangicYetki3}> - <@&${auth.YetkiliAlım.AltYetki}>)
+\`.dreid [@Kullanıcı]\` (<@&${auth.YetkiliAlım.BaslangicYetki4}> - <@&${auth.YetkiliAlım.AltYetki}>)
+\`.shadow [@Kullanıcı]\` (<@&${auth.YetkiliAlım.BaslangicYetki5}> - <@&${auth.YetkiliAlım.AltYetki}> - <@&${auth.YetkiliAlım.BotCommands}>)
+
+⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+**Argümanlar:**
+\`.rise\`: **.ytver [@Kullanıcı]**
+\`.moon\`: **.ytver2 [@Kullanıcı]**
+\`.shade\`: **.ytver3 [@Kullanıcı]**
+\`.dreid\`: **.ytver4 [@Kullanıcı]**
+\`.shadow\`: **.ytver5 [@Kullanıcı]**
 `,}});
     client.channels.cache.get(auth.Logs.KomutLog).send(`⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n${auth.Reacts.star} ${msg.author.tag}(\`${msg.author.id}\`) kullanıcısı <#${msg.channel.id}> kanalında bir komut kullandı.\n**Komutun içeriği:** \`${msg.content}\``)
 };
