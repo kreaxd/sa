fe5ed00232865c3d2f27f0b518617958c89952ca
Glitch (tillahmoderasyon) <none>
2021-04-13 07:45:05 +0000

    🏊🏼🎿 Checkpoint ./watch.json:1901910/2

--- LoadingCommands/Yardım.js
+++ LoadingCommands/Yardım.js
@@ -1,6 +1,6 @@
 module.exports.operate = async ({client, msg, args, member ,author, auth}, {MessageEmbed} = require("discord.js")) => { 
   run: async (client, message, args, embed, prefix) => {
-    message.channel.send(client.commands.filter((x) => x.conf.help).sort((a, b) => b.conf.help - a.conf.help).map((x) => `\`${prefix}${x.conf.help}\``).join("\n")));
+    message.channel.send(client.commands.filter((x) => x.conf.help).sort((a, b) => b.conf.help - a.conf.help).map((x) => `\`${prefix}${x.conf.help}\``).join("\n"));
   }
 };
   
