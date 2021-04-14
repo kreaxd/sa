79303af09678a8cf61bbe8b8d994289ccae0340b
Glitch (tillahmoderasyon) <none>
2021-04-14 17:55:02 +0000

    🐀🌧 Checkpoint ./LoadingCommands/MuteVoiceToplu.js:1901910/1636 ./LoadingCommands/MuteVoiceTopluKaldır.js:1901910/2503 ./watch.json:1901910/2

--- LoadingCommands/MuteVoiceTopluKaldır.js
+++ LoadingCommands/MuteVoiceTopluKaldır.js
@@ -2,13 +2,12 @@ module.exports.operate = async ({client, msg, args, member, auth, author}, ms = 
     if ((!author.roles.cache.some(r => auth.Perms.VMuteAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
     let channel = msg.guild.channels.cache.get(args[0]) || msg.member.voice.channel;
     if (!channel) return msg.channel.error(msg, "Bir kanal ID girmeli ya da bir sesli kanalda bulunmalısın!");
-    channel.members.filter((x) => !x.permissions.has("ADMINISTRATOR"))
-      .forEach((x, index) => {
-        x.voice.setMute(true);
-      });
-    msg.channel.send(`\`${channel.name}\` kanalındaki tüm üyeler susturuldu!`);
+    channel.members.forEach((x, index) => {
+        x.voice.setMute(false);
+    })
+    msg.channel.send(`\`${channel.name}\` kanalındaki tüm üyelerin susturulması kaldırıldı!`);
   }
 module.exports.help = {
- name: "toplu-sustur",
- alias: ["toplumute"]
+ name: "toplu-unsustur",
+ alias: ["untoplumute"]
 };
