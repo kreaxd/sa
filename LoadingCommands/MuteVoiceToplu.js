a9b8c9a97449694f73d6d97f83d38b4766355210
Glitch (tillahmoderasyon) <none>
2021-04-14 17:43:49 +0000

    🎭📡 Checkpoint ./LoadingCommands/MuteVoiceToplu.js:1901910/5714 ./watch.json:1901910/12 ./LoadingCommands/Rolsüüz.js:1901910/3690

--- LoadingCommands/MuteVoiceToplu.js
+++ LoadingCommands/MuteVoiceToplu.js
@@ -1,15 +1,13 @@
 module.exports.operate = async ({client, msg, args, member, auth, author}, ms = require("ms"), Discord = require("discord.js"), Database = require("../Models/Restriction.js"), Schema = require("../Models/Member.js")) => {
-    if ((!author.roles.cache.some(r => auth.Perms.MuteAuth.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
+    if (!msg.member.hasPermission("MOVE_MEMBERS")) return;
     let channel = msg.guild.channels.cache.get(args[0]) || msg.member.voice.channel;
-    if (!channel) return msg.channel.error(msg, "Bir kanal idsi girmen gerekiyor.");
-    channel.members.filter((x) => !x.hasPermissions("ADMINISTRATOR"))
-    .forEach((x, index) => {
-        client.wait(index * 1000);
+    if (!channel) return msg.channel.error(msg, "Bir kanal ID girmeli ya da bir sesli kanalda bulunmalısın!");
+    channel.members.filter((x) => !x.hasPermission("ADMINISTRATOR"))
+      .forEach((x, index) => {
         x.voice.setMute(true);
-    });
+      }, X * 750);
     msg.channel.send(`\`${channel.name}\` kanalındaki tüm üyeler susturuldu!`);
-}
-
+  }
 module.exports.help = {
  name: "toplu-sustur",
  alias: ["toplumute"]
