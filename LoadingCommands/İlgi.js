611ec613307c46b90962f00baf076ad46550d9ee
Glitch (tillahmoderasyon) <none>
2021-04-15 00:53:33 +0000

    🏙📙 Checkpoint ./LoadingCommands/İlgi.js:1901910/1253 ./Models/Member.js:1901910/83 ./LoadingCommands/ilgiBilgi.js:1901910/2191

--- LoadingCommands/İlgi.js
+++ LoadingCommands/İlgi.js
@@ -1,19 +1,34 @@
 let ilgiSize = new Set();
-module.exports.operate = async ({client, msg, author, member, auth}, Discord = require("discord.js")) => {
-      if (!ilgiSize[author.id])
+module.exports.operate = async ({client, msg, args, member, auth, author}, ms = require("ms"), Database = require("../Models/Restriction.js"), Schema = require("../Models/Member.js")) => {
+    let VeriNumber = 0;   
+    let Sayi;
+  if (!ilgiSize[author.id])
         ilgiSize[author.id] = { kullanim: 0};
     if (ilgiSize[author.id].kullanim >= 5) return client.message(client.embed(`Bir gün içinde maximum 5 kişiye ilgi verebilirsin.`, msg), msg.channel.id, 6500);
   if (!member) return client.message(client.noMember(msg), msg.channel.id, 6500);
     msg.react(client.react("duztik"));
     member.roles.add("832050058743775260")
     client.message(`:cookie: ${author} tarafından ${member} adlı **ibibikine** ilgi verildi.`, msg.channel.id, 7500);
+    Database.countDocuments().then(async x => {
+  VeriNumber = ( x + 1 );
+  Schema.findOne({SunucuID: msg.guild.id, userID: member.id}, async (err, res) => {
+    if (!res) {
+      Sayi = 4;
+      new Schema({SunucuID: msg.guild.id , userID: member.id, ilgiPuan: 4}).save();
+    } else {
+      res.ilgiPuan = Number(res.ilgiPuan + 4);
+      res.save();
+      Sayi = res.ilgiPuan;
+      }
       ilgiSize[author.id].kullanim++;
     setTimeout(() => {
         if (ilgiSize[author.id].kullanim >= 4) {
             ilgiSize[author.id].kullanim = 0;
         }
     }, 86400000);
-  };
+    })
+    })
+}
 
   module.exports.help = {
     name: "ilgi",
--- Models/Member.js
+++ Models/Member.js
@@ -8,6 +8,7 @@ const member = mongoose.Schema({
   Authorized: { Man: { type: Number, default: 0 }, Woman: { type: Number, default: 0 }, Members: Array, Tags: { type: Number, default: 0 }, TagMembers: Array },
   RestNumber: { BanNumber: { type: Number, default: 0 }, JailNumber: { type: Number, default: 0 }, VMuteNumber: { type: Number, default: 0 }, MuteNumber: { type: Number, default: 0 } },
   CezaPuan: { type: Number, default: 0},
+  ilgiPuan: { type: Number, default: 0},
   Uyarılar: Array,
   Nots: Array
 });
