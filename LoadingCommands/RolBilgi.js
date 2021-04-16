3ee54beb0f996bbd847481d2a586038fa4e87505
Glitch (tillahmoderasyon) <none>
2021-04-16 16:47:58 +0000

    🐇👼 Checkpoint ./watch.json:1208823/6 ./LoadingCommands/RolBilgi.js:1208823/72

--- LoadingCommands/RolBilgi.js
+++ LoadingCommands/RolBilgi.js
@@ -1,5 +1,5 @@
 module.exports.operate = async ({client, msg, args, author, auth}, Discord = require("discord.js"), moment = require("moment")) => {
-    if (msg.channel.id !== "826924665972588567") return msg.channel.send("sadece bu kanalda çalışan bir komut <#826924665972588567>");
+    if (msg.channel.id !== "832636695513006110") return msg.channel.send("sadece bu kanalda çalışan bir komut <#832636695513006110>");
     if ((!author.roles.cache.some(r => auth.Perms.Yonetim.includes(r.id))) && (!author.permissions.has("ADMINISTRATOR"))) return;
     let rol = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[0]);
     if (!rol) return msg.channel.send({ embed: { author: { name: msg.guild.name, icon_url: msg.guild.iconURL({ dynamic: true }) }, description: `**Bir rol belirtmelisin.** \`@Rol yada ID\``, color: client.renk[Math.floor(Math.random() * client.renk.length)] } });
