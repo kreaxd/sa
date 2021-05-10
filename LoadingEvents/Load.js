df4504ed17cd8093cc64d7218b4d8616e5af72ea
Glitch (tillahmoderasyon) <none>
2021-05-10 10:52:39 +0000

    🌍🍕 Checkpoint ./LoadingEvents/Load.js:1208823/35 ./watch.json:1208823/2

--- LoadingEvents/Load.js
+++ LoadingEvents/Load.js
@@ -6,7 +6,7 @@ class Ready {
     this.guild = guild;
   }
   kur() {
-     client.user.setActivity("Phedora ❤️ ibidi.");
+     client.user.setActivity("ibidi ❤️ Muratva Stark");
     console.log("("+client.user.username +") adlı hesapta [" +this.guild.name+"] adlı sunucuda giriş yapıldı.");
   }
 }
